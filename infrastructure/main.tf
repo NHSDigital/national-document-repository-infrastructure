44163ae7d46b1f2cf664ee746037ef1b0badcf46
Rich Byrne <127743021+richbyrne-nhs@users.noreply.github.com>
2023-07-25 17:15:39 +0100

    Created initial s3 module and workspace (#1)
    * [NRD-2] Created an initial S3 module
    Co-authored-by: Rich Byrne <richard.byrne2@nhs.net>
    Co-authored-by: Rio Knightley <rio.knightley2@nhs.net>
    
    * [NRD-2] Created an initial workspace
    Co-authored-by: Rich Byrne <richard.byrne2@nhs.net>
    Co-authored-by: Rio Knightley <rio.knightley2@nhs.net>
    
    ---------
    
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>

---
+++ infrastructure/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+  backend "s3" {
+    dynamodb_table = "ndr-terraform-locks"
+    region         = "eu-west-2"
+    key            = "ndr/terraform.tfstate"
+    encrypt        = true
+  }
+}
+provider "aws" {
+  region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
