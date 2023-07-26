18000992c6d5864ec60a572efd37bf133d54525c
NogaNHS <noga.sasson1@nhs.net>
2023-07-26 13:49:23 +0100

    [NRD-3] added missing commas

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -34,32 +34,32 @@ resource "aws_ecr_lifecycle_policy" "ndr_ecr_lifecycle_policy" {
 
 resource "aws_ecr_repository_policy" "ndr_ecr_repository_policy" {
   repository = aws_ecr_repository.ndr-docker-ui.name
-  policy     = <<EOF
-  {
-      "Version": "2008-10-17",
-      "Statement": [
-          {
-              "Sid": "1",
-              "Effect": "Allow",
-              "Principal": "*",
-              "Action": [
-                  "ecr:GetDownloadUrlForLayer",
-                  "ecr:BatchGetImage",
-                  "ecr:BatchCheckLayerAvailability",
-                  "ecr:PutImage",
-                  "ecr:InitiateLayerUpload",
-                  "ecr:UploadLayerPart",
-                  "ecr:CompleteLayerUpload"
-                  "ecr:DescribeRepositories"
-                  "ecr:GetRepositoryPolicy",
-                  "ecr:ListImages",
-                  "ecr:DeleteRepository",
-                  "ecr:BatchDeleteImage",
-                  "ecr:SetRepositoryPolicy",
-                  "ecr:DeleteRepositoryPolicy"
-              ]
-          }
-      ]
-  }
-  EOF
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "1",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
 }
