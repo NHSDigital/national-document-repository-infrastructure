e2eda46efa4945ee566b3d73570d470328a90f66
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2023-08-07 16:11:43 +0100

    [PRMDR-3] Created an ECR module (#2)
    * [NRD-2] Created an initial ecr module
    Co-authored-by: Rich Byrne <richard.byrne2@nhs.net>
    Co-authored-by: Rio Knightley <rio.knightley2@nhs.net>

---
+++ infrastructure/modules/ecr/main.tf
@@ -0,0 +1,71 @@
+resource "aws_ecr_repository" "ndr-docker-ui" {
+  name                 = var.app_name
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  encryption_configuration {
+    encryption_type = "KMS"
+  }
+  tags = {
+    Name        = "${terraform.workspace}-${var.app_name}"
+    Owner       = var.owner
+    Environment = var.environment
+    Workspace   = terraform.workspace
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "ndr_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.ndr-docker-ui.name
+  policy     = <<EOF
+  {
+      "rules": [
+          {
+              "rulePriority": 1,
+              "description": "Expire images older than 7 days",
+              "selection": {
+                  "tagStatus": "untagged",
+                  "countType": "sinceImagePushed",
+                  "countUnit": "days",
+                  "countNumber": 7
+              },
+              "action": {
+                  "type": "expire"
+              }
+          }
+      ]
+  }
+  EOF
+}
+
+resource "aws_ecr_repository_policy" "ndr_ecr_repository_policy" {
+  repository = aws_ecr_repository.ndr-docker-ui.name
+  policy     = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "1",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
+            ]
+        }
+    ]
+}
+EOF
+}
---
+++ infrastructure/modules/ecr/variable.tf
@@ -0,0 +1,12 @@
+variable "app_name" {
+  description = "the name of the app"
+  type        = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "owner" {
+  type = string
+}
