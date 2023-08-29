28f8b2f2fcbd92ebad6e354b02c2050f68d0f951
NogaNHS <noga.sasson1@nhs.net>
2023-08-29 16:40:13 +0100

    [PRMDR-68] change bucket CORS policy

--- infrastructure/buckets.tf
+++ infrastructure/buckets.tf
@@ -2,7 +2,7 @@
 module "ndr-document-store" {
   source                    = "./modules/s3/"
   bucket_name               = var.docstore_bucket_name
-  origin                    = "'https://${terraform.workspace}.${var.domain}'"
+  origin                    = "https://${terraform.workspace}.${var.domain}"
   enable_cors_configuration = true
   environment               = var.environment
   owner                     = var.owner
