746359fa36ca20375d08e81cec71871289bee2d5
NogaNHS <noga.sasson1@nhs.net>
2023-08-30 09:43:10 +0100

    [PRMDR-68] changing cors rule to a dynamic resource

--- infrastructure/buckets.tf
+++ infrastructure/buckets.tf
@@ -2,9 +2,21 @@
 module "ndr-document-store" {
   source                    = "./modules/s3/"
   bucket_name               = var.docstore_bucket_name
-  origin                    = "https://${terraform.workspace}.${var.domain}"
   enable_cors_configuration = true
   environment               = var.environment
   owner                     = var.owner
   force_destroy             = contains(["ndra", "ndrb", "ndr-test"], terraform.workspace)
+  cors_rules = [
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["POST", "DELETE"]
+      allowed_origins = ["https://${terraform.workspace}.${var.domain}"]
+      expose_headers  = ["ETag"]
+      max_age_seconds = 3000
+    },
+    {
+      allowed_methods = ["GET"]
+      allowed_origins = ["https://${terraform.workspace}.${var.domain}"]
+    }
+  ]
 }
