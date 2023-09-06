e14543acf64e4b2853f5af2b94b2b2f519c055b1
RachelHowellNHS <rachel.howell6@nhs.net>
2023-09-06 12:15:36 +0100

    [PRMDR-214] create new dynamodb table for zipped record requests

--- infrastructure/dynamo_db.tf
+++ infrastructure/dynamo_db.tf
@@ -78,3 +78,43 @@ module "lloyd_george_reference_dynamodb_table" {
   owner       = var.owner
 }
 
+module "zip_store_reference_dynamodb_table" {
+  source                      = "./modules/dynamo_db"
+  table_name                  = var.zip_store_dynamodb_table_name
+  hash_key                    = "ID"
+  deletion_protection_enabled = false
+  stream_enabled              = false
+  ttl_enabled                 = false
+
+  attributes = [
+    {
+      name = "ID"
+      type = "S"
+    },
+    {
+      name = "FileLocation"
+      type = "S"
+    },
+    {
+      name = "NhsNumber"
+      type = "S"
+    }
+  ]
+
+  global_secondary_indexes = [
+    {
+      name            = "FileLocationsIndex"
+      hash_key        = "FileLocation"
+      projection_type = "ALL"
+    },
+    {
+      name            = "NhsNumberIndex"
+      hash_key        = "NhsNumber"
+      projection_type = "ALL"
+    }
+  ]
+
+  environment = var.environment
+  owner       = var.owner
+}
+
