c0a1929c80d930e4a5bac31a23382a7d8b6aa2d2
Alex Herbert <127327856+AlexHerbertNHS@users.noreply.github.com>
2023-09-06 12:07:52 +0100

    [PRMDR-213] (#34)
    Adding new bucket to store zip files requested by a user

--- infrastructure/buckets.tf
+++ infrastructure/buckets.tf
@@ -21,6 +21,29 @@ module "ndr-document-store" {
   ]
 }
 
+# Zip Request Store Bucket
+module "ndr-zip-request-store" {
+  source                    = "./modules/s3/"
+  bucket_name               = var.zip_store_bucket_name
+  enable_cors_configuration = true
+  environment               = var.environment
+  owner                     = var.owner
+  force_destroy             = contains(["ndra", "ndrb", "ndr-test"], terraform.workspace)
+  cors_rules = [
+    {
+      allowed_headers = ["*"]
+      allowed_methods = ["POST", "DELETE"]
+      allowed_origins = ["https://${terraform.workspace}.${var.domain}"]
+      expose_headers  = ["ETag"]
+      max_age_seconds = 3000
+    },
+    {
+      allowed_methods = ["GET"]
+      allowed_origins = ["https://${terraform.workspace}.${var.domain}"]
+    }
+  ]
+}
+
 # Lloyd George Store Bucket
 module "ndr-lloyd-george-store" {
   source                    = "./modules/s3/"
