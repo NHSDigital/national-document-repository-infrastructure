5ae9cbd691db82f792f285ed68aef5bbd7fc9cae
Rio Knightley <rio.knightley2@nhs.net>
2023-09-11 14:00:44 +0100

    Attach IAM policy for sqs

--- infrastructure/modules/sqs/main.tf
+++ infrastructure/modules/sqs/main.tf
@@ -8,3 +8,28 @@ resource "aws_sqs_queue" "sqs_queue" {
   receive_wait_time_seconds  = var.receive_wait
   sqs_managed_sse_enabled    = var.enable_sse
 }
+
+data "aws_iam_policy_document" "sqs_queue_policy" {
+  statement {
+    sid    = "shsqsstatement"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "sqs:SendMessage",
+      "sqs:ReceiveMessage"
+    ]
+    resources = [
+      aws_sqs_queue.sqs_queue.arn
+    ]
+  }
+}
+
+resource "aws_sqs_queue_policy" "sqs_queue_policy" {
+  queue_url = aws_sqs_queue.sqs_queue.id
+  policy    = data.aws_iam_policy_document.sqs_queue_policy.json
+}
