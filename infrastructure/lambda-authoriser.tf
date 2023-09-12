65ea73c076c0e7f11c093e5851bbf657c1a85b1a
NogaNHS <noga.sasson1@nhs.net>
2023-09-12 08:58:28 +0100

    [PRMDR-163] remove comment Co-authored-by: Joe Fong <joe.fong1@nhs.net>

--- infrastructure/lambda-authoriser.tf
+++ infrastructure/lambda-authoriser.tf
@@ -22,7 +22,6 @@ resource "aws_api_gateway_authorizer" "repo_authoriser" {
   identity_source = "method.request.header.Authorization"
   rest_api_id     = aws_api_gateway_rest_api.ndr_doc_store_api.id
   authorizer_uri  = module.authoriser-lambda.invoke_arn
-  #   authorizer_credentials           = aws_iam_role.authoriser_execution.arn
 }
 
 resource "aws_iam_policy" "ssm_policy_authoriser" {
