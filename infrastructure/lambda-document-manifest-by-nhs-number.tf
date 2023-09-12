fde332b43d0acc48dac7a87b29f0414687880bfe
Scott Alexander <scott.alexander@madetech.com>
2023-09-12 10:53:24 +0100

    make format fix

--- infrastructure/lambda-document-manifest-by-nhs-number.tf
+++ infrastructure/lambda-document-manifest-by-nhs-number.tf
@@ -38,7 +38,7 @@ module "document-manifest-by-nhs-number-lambda" {
   api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
   lambda_environment_variables = {
     DOCUMENT_STORE_BUCKET_NAME   = "${terraform.workspace}-${var.docstore_bucket_name}"
-    LLOYD_GEORGE_BUCKET_NAME   = "${terraform.workspace}-${var.lloyd_george_bucket_name}"
+    LLOYD_GEORGE_BUCKET_NAME     = "${terraform.workspace}-${var.lloyd_george_bucket_name}"
     DOCUMENT_STORE_DYNAMODB_NAME = "${terraform.workspace}_DocumentReferenceMetadata"
     LLOYD_GEORGE_DYNAMODB_NAME   = "${terraform.workspace}_${var.lloyd_george_dynamodb_table_name}"
   }
