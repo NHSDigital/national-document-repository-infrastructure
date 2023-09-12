0c3c4f7c10dde334106469d22e748a89fbdf4015
Rio Knightley <rio.knightley2@nhs.net>
2023-09-12 11:12:28 +0100

    Add initial alarm module

--- infrastructure/alarms.tf
+++ infrastructure/alarms.tf
@@ -1,8 +1,83 @@
-module "badrequest-alarm" {
-  source        = "./modules/alarm"
-  filter_name   = "BadRequestFilter"
-  alarm_name    = "bad-request"
-  error_code    = "BAD_REQUEST"
-  # alarm_actions       = [var.sns_topic_arn]
 
+module "5xx-alarm" {
+  source        = "./modules/alarm"
+  alarm_name    = "5xx_error"
+  alarm_description = "Triggers when a 5xx status code has been returned by the DocStoreAPI." 
+  namespace = "AWS/ApiGateway"
+  api_name = module.aws_api_gateway_rest_api.lambda_api.name
+  metric_name = "5XXError"
+  alarm_actions = [aws_sns_topic.repo_alarm_notifications.arn] 
+  ok_actions = [aws_sns_topic.repo_alarm_notifications.arn] 
+}
+
+
+resource "aws_sns_topic" "repo_alarm_notifications" {
+  name = "alarms-notifications-topic"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudwatch.amazonaws.com"
+        },
+        "Action" : "SNS:Publish",
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "arn:aws:cloudwatch:${var.region}:${var.account_id}:alarm:*"
+          }
+        }
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_kms_key" "alarm_notification_encryption_key" {
+  description         = "Custom KMS Key to enable server side encryption for alarm notifications"
+  policy              = data.aws_iam_policy_document.alarm_notification_kms_key_policy_doc.json
+  enable_key_rotation = true
+}
+
+resource "aws_kms_alias" "alarm_notification_encryption_key_alias" {
+  name          = "alias/alarm-notification-encryption-key-kms-${terraform.workspace}"
+  target_key_id = aws_kms_key.alarm_notification_encryption_key.id
+}
+
+
+data "aws_iam_policy_document" "alarm_notification_kms_key_policy_doc" {
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["arn:aws:iam::${var.account_id}:root"]
+      type        = "AWS"
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["sns.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["cloudwatch.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*"
+    ]
+    resources = ["*"]
+  }
 }
--- infrastructure/modules/alarm/main.tf
+++ infrastructure/modules/alarm/main.tf
@@ -1,24 +1,17 @@
-resource "aws_cloudwatch_log_metric_filter" "error-filter" {
-  name           = var.filter_name //BadRequestFilter
-  log_group_name = aws_cloudwatch_log_group.job-runner-cloudwatch-log-group.name
-  pattern        = var.error_code //"BAD_REQUEST"
-  metric_transformation {
-    name      = "${var.error_code}-Metric"
-    namespace = "${terraform.workspace}-RepoMetrics"
-    value     = "1"
+resource "aws_cloudwatch_metric_alarm" "repo_alarm" {
+  alarm_name        = "${terraform.workspace}_repo_${var.alarm_name}"   
+  alarm_description = var.alarm_description
+  namespace         = "AWS/ApiGateway"
+  dimensions = {
+    ApiName = var.api_name
   }
-}
-
-resource "aws_cloudwatch_metric_alarm" "error-alarm" {
-  alarm_name          = var.alarm_name //bad-request
-  alarm_description = "Triggers when a 5xx status code has been returned by the DocStoreAPI."
-  metric_name         = aws_cloudwatch_log_metric_filter.error-we-care-about-metric-filter.name
-  threshold           = "0"
-  statistic           = "Sum"
+  metric_name         = var.metric_name
   comparison_operator = "GreaterThanThreshold"
-  datapoints_to_alarm = "1"
+  threshold           = "0"
+  period              = "300"
   evaluation_periods  = "1"
-  period              = "60"
-  namespace           = "${terraform.workspace}-RepoMetrics"
+  statistic           = "Sum"
+  actions_enabled     = "true"
   alarm_actions       = var.alarm_actions
+  ok_actions          = var.ok_actions
 }
