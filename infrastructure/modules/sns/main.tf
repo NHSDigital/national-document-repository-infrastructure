7874df5bb98af89761e0c65df5d28c02b1eeb5cb
Rio Knightley <rio.knightley2@nhs.net>
2023-09-12 15:55:06 +0100

    Fix policy error

--- infrastructure/modules/sns/main.tf
+++ infrastructure/modules/sns/main.tf
@@ -1,6 +1,6 @@
 resource "aws_sns_topic" "sns_topic" {
   name                        = "${terraform.workspace}-sns-${var.topic_name}"
-  delivery_policy             = var.delivery_policy
+  policy                      = var.delivery_policy
   fifo_topic                  = var.enable_fifo
   content_based_deduplication = var.enable_deduplication
 }
