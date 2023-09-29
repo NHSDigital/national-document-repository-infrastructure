b8f9490fe0b81e9e83d211178e036b958861bd0e
scott alexander <scott.r.alexander@me.com>
2023-09-29 13:43:36 +0100

    Corrected DYNAMODB_TABLE_LIST format (#54)
    Co-authored-by: Scott Alexander <scott.alexander@madetech.com>

--- infrastructure/lambda-search-doc-references.tf
+++ infrastructure/lambda-search-doc-references.tf
@@ -75,7 +75,7 @@ module "search-document-references-lambda" {
   http_method       = "GET"
   api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
   lambda_environment_variables = {
-    DYNAMODB_TABLE_LIST = "[${terraform.workspace}_${var.docstore_dynamodb_table_name}, ${terraform.workspace}_${var.lloyd_george_dynamodb_table_name}]"
+    DYNAMODB_TABLE_LIST = "[\u0022${terraform.workspace}_${var.docstore_dynamodb_table_name}\u0022, \u0022${terraform.workspace}_${var.lloyd_george_dynamodb_table_name}\u0022]"
   }
   depends_on = [
     aws_api_gateway_rest_api.ndr_doc_store_api,
