6e8d40f7ff4746f0d2ad294a3e11c0b09ad4c82f
NogaNHS <noga.sasson1@nhs.net>
2023-10-05 14:13:53 +0100

    prmdr-161 rebase fixes 2

--- infrastructure/lambda-lloyd-george-record-stitch.tf
+++ infrastructure/lambda-lloyd-george-record-stitch.tf
@@ -1,14 +1,14 @@
 module "lloyd-george-stitch-gateway" {
   # Gateway Variables
-  source                   = "./modules/gateway"
-  api_gateway_id           = aws_api_gateway_rest_api.ndr_doc_store_api.id
-  parent_id                = aws_api_gateway_rest_api.ndr_doc_store_api.root_resource_id
-  http_method              = "GET"
-  authorization            = "NONE" // "CUSTOM"
-  gateway_path             = "LloydGeorgeStitch"
-  authorizer_id            = null
-  cors_require_credentials = var.cors_require_credentials
-  origin                   = "'https://${terraform.workspace}.${var.domain}'"
+  source              = "./modules/gateway"
+  api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  parent_id           = aws_api_gateway_rest_api.ndr_doc_store_api.root_resource_id
+  http_method         = "GET"
+  authorization       = "CUSTOM"
+  gateway_path        = "LloydGeorgeStitch"
+  authorizer_id       = null
+  require_credentials = true
+  origin              = "'https://${terraform.workspace}.${var.domain}'"
 
   # Lambda Variables
   api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
