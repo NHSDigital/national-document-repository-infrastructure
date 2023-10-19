9c130799d6f387ff46c9b2c0c584a1d7530a396a
Richard Smith <43441481+thisusernameisnowtaken@users.noreply.github.com>
2023-10-19 17:03:32 +0100

    PRMDR-168 Back Channel Logout (#70)
    Back Channel Logout infra plus some correction to the Logout Infra
    
    ---------
    
    Co-authored-by: AlexHerbertNHS <alexandra.herbert1@nhs.net>

--- infrastructure/lambda-back-channel-logout.tf
+++ infrastructure/lambda-back-channel-logout.tf
@@ -1,11 +1,11 @@
-module "logout-gateway" {
+module "back-channel-logout-gateway" {
   # Gateway Variables
   source              = "./modules/gateway"
   api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
   parent_id           = aws_api_gateway_resource.auth_resource.id
-  http_method         = "GET"
+  http_method         = "POST"
   authorization       = "NONE"
-  gateway_path        = "Logout"
+  gateway_path        = "BackChannelLogout"
   require_credentials = false
   origin              = "'https://${terraform.workspace}.${var.domain}'"
   # Lambda Variables
@@ -18,10 +18,10 @@ module "logout-gateway" {
   ]
 }
 
-module "logout_lambda" {
+module "back_channel_logout_lambda" {
   source  = "./modules/lambda"
-  name    = "LogoutHandler"
-  handler = "handlers.logout_handler.lambda_handler"
+  name    = "BackChannelLogoutHandler"
+  handler = "handlers.back_channel_logout_handler.lambda_handler"
   iam_role_policies = [
     "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
     "arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy",
@@ -29,39 +29,39 @@ module "logout_lambda" {
     module.auth_session_dynamodb_table.dynamodb_policy
   ]
   rest_api_id       = aws_api_gateway_rest_api.ndr_doc_store_api.id
-  resource_id       = module.logout-gateway.gateway_resource_id
-  http_method       = "GET"
+  resource_id       = module.back-channel-logout-gateway.gateway_resource_id
+  http_method       = "POST"
   api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
   lambda_environment_variables = {
     WORKSPACE                      = terraform.workspace
     AUTH_DYNAMODB_NAME             = "${terraform.workspace}_${var.auth_session_dynamodb_table_name}"
     SSM_PARAM_JWT_TOKEN_PUBLIC_KEY = "jwt_token_public_key"
-
+    OIDC_CALLBACK_URL              = "https://${terraform.workspace}.${var.domain}/auth-callback"
   }
   depends_on = [
     aws_api_gateway_rest_api.ndr_doc_store_api,
     aws_iam_policy.ssm_policy_oidc,
     module.auth_session_dynamodb_table,
-  module.logout-gateway]
+  module.back-channel-logout-gateway]
 }
 
-module "logout_alarm" {
+module "back_channel_logout_alarm" {
   source               = "./modules/alarm"
-  lambda_function_name = module.logout_lambda.function_name
-  lambda_timeout       = module.logout_lambda.timeout
+  lambda_function_name = module.back_channel_logout_lambda.function_name
+  lambda_timeout       = module.back_channel_logout_lambda.timeout
   lambda_name          = "logout_handler"
   namespace            = "AWS/Lambda"
-  alarm_actions        = [module.logout_alarm_topic.arn]
-  ok_actions           = [module.logout_alarm_topic.arn]
-  depends_on           = [module.logout_lambda, module.logout_alarm_topic]
+  alarm_actions        = [module.back_channel_logout_alarm_topic.arn]
+  ok_actions           = [module.back_channel_logout_alarm_topic.arn]
+  depends_on           = [module.back_channel_logout_lambda, module.back_channel_logout_alarm_topic]
 }
 
 
-module "logout_alarm_topic" {
+module "back_channel_logout_alarm_topic" {
   source         = "./modules/sns"
-  topic_name     = "logout-alarms-topic"
+  topic_name     = "back-channel-logout-alarms-topic"
   topic_protocol = "lambda"
-  topic_endpoint = module.logout_lambda.endpoint
+  topic_endpoint = module.back_channel_logout_lambda.endpoint
   delivery_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
@@ -83,5 +83,5 @@ module "logout_alarm_topic" {
     ]
   })
 
-  depends_on = [module.logout_lambda]
+  depends_on = [module.back_channel_logout_lambda]
 }
