a4fe7e32445e68884387513553e4a141ba9cbc6a
Rio Knightley <rio.knightley2@nhs.net>
2023-10-23 15:34:40 +0100

    Fix lambda arn output error

--- infrastructure/schedules.tf
+++ infrastructure/schedules.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_event_rule" "bulk_upload_report_schedule" {
 resource "aws_cloudwatch_event_target" "bulk_upload_report_schedule_event" {
   rule      = aws_cloudwatch_event_rule.bulk_upload_report_schedule.name
   target_id = "processing_lambda"
-  arn       = module.bulk-upload-report-lambda.lambda.arn
+  arn       = module.bulk-upload-report-lambda.endpoint
   depends_on = [
     module.bulk-upload-report-lambda,
     aws_cloudwatch_event_rule.bulk_upload_report_schedule
@@ -18,7 +18,7 @@ resource "aws_cloudwatch_event_target" "bulk_upload_report_schedule_event" {
 resource "aws_lambda_permission" "bulk_upload_report_schedule_permission" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = module.bulk-upload-report-lambda.lambda.function_name
+  function_name = module.bulk-upload-report-lambda.function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.bulk_upload_report_schedule.arn
   depends_on = [
