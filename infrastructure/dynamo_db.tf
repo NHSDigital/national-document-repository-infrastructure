6f19225c57cef60dab77ee68df6b90b68dc4812f
Rio Knightley <128376976+RioKnightleyNHS@users.noreply.github.com>
2023-10-23 10:27:20 +0100

    Add bulk upload meta data dynamodb table (#72)

--- infrastructure/dynamo_db.tf
+++ infrastructure/dynamo_db.tf
@@ -171,3 +171,43 @@ module "auth_session_dynamodb_table" {
   environment = var.environment
   owner       = var.owner
 }
+
+module "bulk_upload_dynamodb_table" {
+  source                      = "./modules/dynamo_db"
+  table_name                  = var.bulk_upload_dynamodb_table_name
+  hash_key                    = "ID"
+  deletion_protection_enabled = false
+  stream_enabled              = false
+  ttl_enabled                 = false
+
+  attributes = [
+    {
+      name = "ID"
+      type = "S"
+    },
+    {
+      name = "FileLocation"
+      type = "S"
+    },
+    {
+      name = "NhsNumber"
+      type = "S"
+    }
+  ]
+
+  global_secondary_indexes = [
+    {
+      name            = "FileLocationsIndex"
+      hash_key        = "FileLocation"
+      projection_type = "ALL"
+    },
+    {
+      name            = "NhsNumberIndex"
+      hash_key        = "NhsNumber"
+      projection_type = "ALL"
+    }
+  ]
+
+  environment = var.environment
+  owner       = var.owner
+}
