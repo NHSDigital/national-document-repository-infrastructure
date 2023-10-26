b5d82129b003d9145a59912af12d8fd3e63d7108
scott alexander <scott.r.alexander@me.com>
2023-10-26 13:53:27 +0100

    PRMDR firewall has been removed from sandbox envs (#84)
    * the firewall will not be deployed to our sandbox envs going forwards
    
    * pre-commit tidy
    
    ---------
    
    Co-authored-by: Scott Alexander <scott.alexander@madetech.com>

--- infrastructure/README.md
+++ infrastructure/README.md
@@ -8,8 +8,7 @@
 
 | Name | Version |
 |------|---------|
-| <a name="provider_aws"></a> [aws](#provider\_aws) | 5.12.0 |
-
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 5.13.1 |
 
 ## Modules
 
--- infrastructure/firewall.tf
+++ infrastructure/firewall.tf
@@ -3,13 +3,22 @@ module "firewall_waf_v2" {
 
   environment = var.environment
   owner       = var.owner
+  count = (terraform.workspace == "ndra" ||
+    terraform.workspace == "ndrb" ||
+    terraform.workspace == "ndrc" ||
+  terraform.workspace == "ndrd") ? 0 : 1
 }
 
 resource "aws_wafv2_web_acl_association" "web_acl_association" {
   resource_arn = module.ndr-ecs-fargate.load_balancer_arn
-  web_acl_arn  = module.firewall_waf_v2.arn
+  web_acl_arn  = module.firewall_waf_v2[0].arn
+
+  count = (terraform.workspace == "ndra" ||
+    terraform.workspace == "ndrb" ||
+    terraform.workspace == "ndrc" ||
+  terraform.workspace == "ndrd") ? 0 : 1
   depends_on = [
     module.ndr-ecs-fargate,
-    module.firewall_waf_v2
+    module.firewall_waf_v2[0]
   ]
 }
