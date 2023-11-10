c9b12ef8e2737270ab4d4fc1bd2bc2ffb78a4496
RachelHowellNHS <rachel.howell6@nhs.net>
2023-11-10 15:17:01 +0000

    [PRMDR-402] remove autoscaling group name

--- infrastructure/modules/ecs/alarms.tf
+++ infrastructure/modules/ecs/alarms.tf
@@ -8,12 +8,9 @@ resource "aws_cloudwatch_metric_alarm" "alb_alarm_4XX" {
   statistic           = "Sum"
   threshold           = 20
   treat_missing_data  = "notBreaching"
-
   dimensions = {
-    AutoScalingGroupName = aws_lb.ecs_lb.arn_suffix
-    LoadBalancer         = aws_lb.ecs_lb.arn_suffix
+    LoadBalancer = aws_lb.ecs_lb.arn_suffix
   }
-
   alarm_description = "This alarm indicates that at least 20 4XX statuses have occurred on ${aws_lb.ecs_lb.name} in a minute."
   actions_enabled   = "true"
   alarm_actions     = var.alarm_actions_arn_list
@@ -29,12 +26,9 @@ resource "aws_cloudwatch_metric_alarm" "alb_alarm_5XX" {
   statistic           = "Sum"
   threshold           = 5
   treat_missing_data  = "notBreaching"
-
   dimensions = {
-    AutoScalingGroupName = aws_lb.ecs_lb.arn_suffix
-    LoadBalancer         = aws_lb.ecs_lb.arn_suffix
+    LoadBalancer = aws_lb.ecs_lb.arn_suffix
   }
-
   alarm_description = "This alarm indicates that at least 5 5XX statuses have occurred on ${aws_lb.ecs_lb.name} within 5 minutes."
   actions_enabled   = "true"
   alarm_actions     = var.alarm_actions_arn_list
