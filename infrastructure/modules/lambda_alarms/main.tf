838a56cd029108ca8663a891e517d23b69c879a8
RachelHowellNHS <rachel.howell6@nhs.net>
2023-11-16 13:52:09 +0000

    [PRMDR-407] revert lambda memory alarm metric_name and namespace

--- infrastructure/modules/lambda_alarms/main.tf
+++ infrastructure/modules/lambda_alarms/main.tf
@@ -38,8 +38,8 @@ resource "aws_cloudwatch_metric_alarm" "lambda_memory_alarm" {
     function_name = var.lambda_function_name
   }
   threshold           = 80
-  namespace           = "AWS/ECS"
-  metric_name         = "MemoryUtilization"
+  namespace           = "LambdaInsights"
+  metric_name         = "memory_utilization"
   comparison_operator = "GreaterThanThreshold"
   period              = "300"
   evaluation_periods  = "1"
