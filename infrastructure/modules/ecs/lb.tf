03a9cc1a21ee364bd57625fd7b90fecf02d15f5b
Alex Herbert <127327856+AlexHerbertNHS@users.noreply.github.com>
2023-11-16 09:49:27 +0000

    [PRMDR-455] (#94)
    Adding load balancer deletion protection as per feedback from pentest

--- infrastructure/modules/ecs/lb.tf
+++ infrastructure/modules/ecs/lb.tf
@@ -4,6 +4,7 @@ resource "aws_lb" "ecs_lb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ndr_ecs_sg.id]
   subnets            = [for subnet in var.public_subnets : subnet]
+  enable_deletion_protection = true
 
   tags = {
     Name        = "${terraform.workspace}-lb-${var.ecs_cluster_name}"
