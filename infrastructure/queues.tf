a593e60c78e29f67ce16c6f8952db1c0ad4d7e0c
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2023-11-27 12:55:32 +0000

    PRMDR-518 FIFO queue change (#113)
    [PRMDR-518] enabling FIFO for bulk upload

--- infrastructure/queues.tf
+++ infrastructure/queues.tf
@@ -15,13 +15,15 @@ module "sqs-nems-queue" {
 }
 
 module "sqs-lg-bulk-upload-metadata-queue" {
-  source            = "./modules/sqs"
-  name              = "lg-bulk-upload-metadata-queue"
-  max_message       = 256 * 1024        # allow message size up to 256 KB
-  message_retention = 60 * 60 * 24 * 14 # 14 days
-  environment       = var.environment
-  owner             = var.owner
-  max_visibility    = 1020
+  source               = "./modules/sqs"
+  name                 = "lg-bulk-upload-metadata-queue.fifo"
+  max_message          = 256 * 1024        # allow message size up to 256 KB
+  message_retention    = 60 * 60 * 24 * 14 # 14 days
+  environment          = var.environment
+  owner                = var.owner
+  max_visibility       = 1020
+  enable_fifo          = true
+  enable_deduplication = true
 }
 
 module "sqs-lg-bulk-upload-invalid-queue" {
