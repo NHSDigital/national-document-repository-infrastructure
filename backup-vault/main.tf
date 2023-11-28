c0553149ce70051b0f3fa6c907bf4441771c411b
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-11-28 14:40:30 +0000

    [PRMDR-533]
    Adding in S3 / Dynamo terraform state storage and locks

--- backup-vault/main.tf
+++ backup-vault/main.tf
@@ -1,13 +1,17 @@
-resource "aws_backup_vault" "backup_vault" {
-  name        = "${terraform.workspace}_backup_vault"
-  kms_key_arn = aws_kms_key.encryption_key.arn
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+  backend "s3" {
+    dynamodb_table = "ndr-backup-terraform-lock"
+    region         = "eu-west-2"
+    key            = "ndr/terraform.tfstate"
+    encrypt        = true
+  }
 }
-
-resource "aws_kms_key" "encryption_key" {
-  description         = "KMS key for encrypting backups"
-  enable_key_rotation = true
-}
-
-resource "aws_kms_alias" "encryption_key_alias" {
-  target_key_id = aws_kms_key.encryption_key.id
+provider "aws" {
+  region = "eu-west-2"
 }
