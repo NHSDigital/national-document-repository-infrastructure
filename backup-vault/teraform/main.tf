af85b8d99ecbfd83b4c49533798024c824c91314
Alex Herbert <127327856+AlexHerbertNHS@users.noreply.github.com>
2023-11-28 15:41:42 +0000

    PRMDR-533 (#114)
    * [PRMDR-533]
    
    Adding a backup vault and a KMS key for encrypting. Deploys well enough on the pre prod backup AWS
    
    * [PRMDR-533]
    
    Formatting
    
    * [PRMDR-533]
    
    Adding in S3 / Dynamo terraform state storage and locks
    
    * [PRMDR-533]
    
    Adding in S3 / Dynamo terraform state storage and locks

---
+++ backup-vault/teraform/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+  backend "s3" {
+    dynamodb_table = "ndr-backup-terraform-lock"
+    region         = "eu-west-2"
+    key            = "ndr/terraform.tfstate"
+    encrypt        = true
+  }
+}
+provider "aws" {
+  region = "eu-west-2"
+}
