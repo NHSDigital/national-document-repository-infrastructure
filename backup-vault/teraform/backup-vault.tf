491689a80fdcdccd1dc6c7d5f1d8e05b433dac0d
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-11-28 16:34:02 +0000

    [PRMDR-531]
    WIP commit

--- backup-vault/teraform/backup-vault.tf
+++ backup-vault/teraform/backup-vault.tf
@@ -11,3 +11,42 @@ resource "aws_kms_key" "encryption_key" {
 resource "aws_kms_alias" "encryption_key_alias" {
   target_key_id = aws_kms_key.encryption_key.id
 }
+
+resource "aws_backup_plan" "example" {
+  name = "tf_example_backup_plan"
+
+  rule {
+    rule_name         = "6pm backup"
+    target_vault_name = aws_backup_vault.backup_vault.name
+    schedule          = "cron(0 18 * * *)"
+
+    lifecycle {
+      delete_after = 35
+    }
+  }
+}
+
+resource "aws_backup_vault_policy" "backup_policy" {
+  backup_vault_name = aws_backup_vault.backup_vault.name
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Id": "default",
+      "Statement": [
+        {
+          "Sid": "Allow production environments ",
+          "Effect": "Allow",
+          "Action": "backup:CopyIntoBackupVault",
+          "Resource": "*",
+          "Principal": {
+            "AWS": "arn:aws:iam::${data.aws_ssm_parameter.backup-target-account}:root"
+          }
+        }
+      ]
+    }
+  )
+}
+
+data "aws_ssm_parameter" "backup-target-account" {
+  name = "backup-target-account"
+}
