284387a70dd434ae4bc29a575e5eac89c9e78aeb
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-11-30 15:35:12 +0000

    [PRMDR-531/532]
    Changing erroneous cron expression

--- infrastructure/backup-cross-account.tf
+++ infrastructure/backup-cross-account.tf
@@ -5,7 +5,7 @@ resource "aws_backup_plan" "cross_account_backup_schedule" {
     rule_name         = "CrossAccount6pmBackup"
     target_vault_name = "${terraform.workspace}_backup_vault"
     #    schedule          = "cron(0 18 * * *)"
-    schedule = "cron(35 15 * * *)"
+    schedule = "cron(45 15 * * ? *)"
 
     lifecycle {
       delete_after       = 35
