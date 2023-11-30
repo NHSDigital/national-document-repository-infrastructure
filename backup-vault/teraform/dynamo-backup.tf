134f4953cad0eea5c4c3985a6e7532920e83835f
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-11-30 10:25:21 +0000

    [PRMDR-531]
    WIP commit

---
+++ backup-vault/teraform/dynamo-backup.tf
@@ -0,0 +1,38 @@
+resource "aws_dynamodb_table" "ehr_backup_table" {
+  name = "electronic-health-record-backup"
+}
+
+resource "aws_dynamodb_table" "lg_backup_table" {
+  name = "lloyd_george_backup"
+}
+
+resource "aws_dynamodb_table" "bulk_upload_report_backup_table" {
+  name = "bulk_upload_report_backup"
+}
+
+resource "aws_iam_policy" "dynamodb_policy" {
+  name = "dynamo_backup_policy"
+  path = "/"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:DeleteItem",
+        ],
+        "Resource" : [
+          aws_dynamodb_table.ehr_backup_table.arn,
+          aws_dynamodb_table.lg_backup_table.arn,
+          aws_dynamodb_table.bulk_upload_report_backup_table.arn,
+        ]
+      }
+    ]
+  })
+}
