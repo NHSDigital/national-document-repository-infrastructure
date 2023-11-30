248fa0e6a7a585de628ae11c854acb1507a87d15
AlexHerbertNHS <alexandra.herbert1@nhs.net>
2023-11-30 14:35:24 +0000

    [PRMDR-531]
    Adding the last of the code required (we think!) to allow S3 resources to be copied into the backup account
    
    Oops, this was actually PRMDR-532

--- backup-vault/teraform/backup-vault.tf
+++ backup-vault/teraform/backup-vault.tf
@@ -12,42 +12,29 @@ resource "aws_kms_alias" "encryption_key_alias" {
   target_key_id = aws_kms_key.encryption_key.id
 }
 
-resource "aws_backup_plan" "backup_schedule" {
-  name = "tf_example_backup_plan"
-
-  rule {
-    rule_name         = "6pm backup"
-    target_vault_name = aws_backup_vault.backup_vault.name
-    schedule          = "cron(0 18 * * *)"
-
-    lifecycle {
-      delete_after = 35
-    }
-  }
-}
 
 resource "aws_backup_vault_policy" "backup_policy" {
   backup_vault_name = aws_backup_vault.backup_vault.name
+
   policy = jsonencode(
     {
-      "Version": "2012-10-17",
-      "Id": "default",
-      "Statement": [
-        {
-          "Sid": "Allow production environments ",
-          "Effect": "Allow",
-          "Action": "backup:CopyIntoBackupVault",
-          "Resource": "*",
-          "Principal": {
-            "AWS": "arn:aws:iam::${data.aws_ssm_parameter.backup-source-account}:root"
-          }
-        }
-      ]
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "Allow ${data.aws_ssm_parameter.backup-source-account} to copy into pre-prod_s3_backup_vault",
+                "Effect": "Allow",
+                "Principal": {
+                    "AWS": "arn:aws:iam::${data.aws_ssm_parameter.backup-source-account}:root"
+                },
+                "Action": "backup:CopyIntoBackupVault",
+                "Resource": "*"
+            }
+        ]
     }
   )
 }
 
 data "aws_ssm_parameter" "backup-source-account" {
-  name = "backup-target-account"
+  name = "backup-source-account"
 }
 
---
+++ infrastructure/backup-cross-account.tf
@@ -0,0 +1,108 @@
+resource "aws_backup_plan" "cross_account_backup_schedule" {
+  name = "${terraform.workspace}-cross-account-backup-plan"
+
+  rule {
+    rule_name         = "CrossAccount6pmBackup"
+    target_vault_name = "${terraform.workspace}_backup_vault"
+    schedule          = "cron(0 18 * * *)"
+
+    lifecycle {
+      delete_after = 35
+      cold_storage_after = 0
+    }
+  }
+}
+
+data "aws_ssm_parameter" "target_backup_vault_arn" {
+  name = "backup-target-vault-arn"
+}
+
+data "aws_ssm_parameter" "backup_target_account" {
+  name = "backup-target-account"
+}
+
+data "aws_iam_policy_document" "cross_account_backup_assume_role" {
+  statement = [
+      {
+        effect = "Allow"
+
+        principals = {
+          type        = "Service"
+          identifiers = ["backup.amazonaws.com"]
+        }
+
+        actions = ["sts:AssumeRole"]
+      },
+      {
+        sid: "Allow 694282683086 to copy into pre-prod_s3_backup_vault",
+        effect: "Allow",
+        action: "backup:CopyIntoBackupVault",
+        resource: data.aws_ssm_parameter.target_backup_vault_arn,
+        principal: {
+          type        = "AWS"
+          identifiers = ["arn:aws:iam::${data.aws_ssm_parameter.backup_target_account}:root"]
+        }
+      }
+  ]
+}
+
+resource "aws_iam_policy" "copy_policy" {
+  name        = "${terraform.workspace}_cross_account_copy_policy"
+  description = "Permissions required to copy to another accounts backup vault"
+  policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "Allow user to copy into backup account",
+              "Effect": "Allow",
+              "Action": "backup:CopyIntoBackupVault",
+              "Resource": data.aws_ssm_parameter.target_backup_vault_arn
+          }
+      ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "s3_cross_account_copy_policy" {
+  role       = aws_iam_role.s3_cross_account_backup_iam_role.name
+  policy_arn = aws_iam_policy.copy_policy.arn
+}
+
+resource "aws_backup_selection" "s3_cross_account_backup_selection" {
+  iam_role_arn = aws_iam_role.s3_cross_account_backup_iam_role.arn
+  name         = "${terraform.workspace}_s3_cross_account_backup_selection"
+  plan_id      = aws_backup_plan.cross_account_backup_schedule.id
+
+  resources = [
+    module.ndr-document-store.bucket_arn,
+    module.ndr-lloyd-george-store.bucket_arn
+  ]
+}
+
+resource "aws_iam_role" "s3_cross_account_backup_iam_role" {
+  name               = "${terraform.workspace}_s3_cross_account_backup_iam_role"
+  assume_role_policy = data.aws_iam_policy_document.backup_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "cross_account_backup_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForBackup"
+  role       = aws_iam_role.s3_cross_account_backup_iam_role.name
+  depends_on = [aws_iam_role.s3_cross_account_backup_iam_role]
+}
+
+resource "aws_iam_role_policy_attachment" "cross_account_restore_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBackupServiceRolePolicyForRestores"
+  role       = aws_iam_role.s3_cross_account_backup_iam_role.name
+  depends_on = [aws_iam_role.s3_cross_account_backup_iam_role]
+}
+
+resource "aws_iam_role_policy_attachment" "cross_account_s3_backup_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSBackupServiceRolePolicyForS3Backup"
+  role       = aws_iam_role.s3_cross_account_backup_iam_role.name
+  depends_on = [aws_iam_role.s3_cross_account_backup_iam_role]
+}
+
+resource "aws_iam_role_policy_attachment" "s3_cross_account_restore_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AWSBackupServiceRolePolicyForS3Restore"
+  role       = aws_iam_role.s3_cross_account_backup_iam_role.name
+  depends_on = [aws_iam_role.s3_cross_account_backup_iam_role]
+}
+
