2bca55f47ee324a748390ede0ad6cf8cf19caf12
NogaNHS <noga.sasson1@nhs.net>
2024-01-08 09:35:39 +0000

    [PRMDR-255] PR changes

--- infrastructure/alarms.tf
+++ infrastructure/alarms.tf
@@ -75,7 +75,7 @@ resource "aws_sns_topic" "alarm_notifications_topic" {
       }
     ]
   })
-  count = local.is_mesh_forwarder_enable ? 1 : 0
+  count = local.is_sandbox ? 1 : 0
 }
 
 resource "aws_sns_topic_subscription" "alarm_notifications_sns_topic_subscription" {
