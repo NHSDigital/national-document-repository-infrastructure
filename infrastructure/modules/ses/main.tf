98396ceb3fb291b7cc47f6a0e2765089b24554ea
abbas-khan10 <abbas.khan10@nhs.net>
2024-01-24 15:36:56 +0000

    add verification

--- infrastructure/modules/ses/main.tf
+++ infrastructure/modules/ses/main.tf
@@ -17,7 +17,13 @@ resource "aws_route53_record" "ndr_ses_dkim_record" {
   records = ["${aws_ses_domain_dkim.ndr_dkim[0].dkim_tokens[count.index]}.dkim.amazonses.com"]
 }
 
-resource "aws_route53_record" "ndr_amazonses_verification_record" {
+resource "aws_ses_domain_identity_verification" "ndr_ses_domain_verification" {
+  domain = aws_ses_domain_identity.ndr_ses[0].domain
+
+  depends_on = [aws_route53_record.ndr_ses_dkim_record]
+}
+
+resource "aws_route53_record" "ndr_ses_verification_record" {
   count   = var.enable ? 1 : 0
   zone_id = var.zone_id
   name    = aws_ses_domain_mail_from.ndr_mail_from[0].mail_from_domain
