9bb00e6fce3fc7e28d43b0244ba401bd14f99244
Joe Fong <joe.fong1@nhs.net>
2024-01-26 12:30:15 +0000

    comment out domain_verification block

--- infrastructure/modules/ses/main.tf
+++ infrastructure/modules/ses/main.tf
@@ -17,12 +17,12 @@ resource "aws_route53_record" "ndr_ses_dkim_record" {
   records = ["${aws_ses_domain_dkim.ndr_dkim[0].dkim_tokens[count.index]}.dkim.amazonses.com"]
 }
 
-resource "aws_ses_domain_identity_verification" "ndr_ses_domain_verification" {
-  domain = aws_ses_domain_identity.ndr_ses[0].domain
-  count  = var.enable ? 1 : 0
+# resource "aws_ses_domain_identity_verification" "ndr_ses_domain_verification" {
+#   domain = aws_ses_domain_identity.ndr_ses[0].domain
+#   count  = var.enable ? 1 : 0
 
-  depends_on = [aws_route53_record.ndr_ses_dkim_record[0]]
-}
+#   depends_on = [aws_route53_record.ndr_ses_dkim_record[0]]
+# }
 
 resource "aws_route53_record" "ndr_amazonses_verification_record" {
   count   = var.enable ? 1 : 0
