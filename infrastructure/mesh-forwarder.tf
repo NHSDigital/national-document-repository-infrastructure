05bf013223bb891a75bf9c556ee3e99c7b9f2eed
abbas-khan10 <127417949+abbas-khan10@users.noreply.github.com>
2024-02-02 12:29:01 +0000

    [FIX] PRMDR-599: Add auto-verification and enable test environment (#142)
    * add verification
    
    * update dkim record
    
    * revert domain prefix in dkim record
    
    * add domain prefix
    
    * remove custom mailing
    
    * re-enable flags
    
    * add fix for nems timeout
    
    * comment out kms for nems
    
    * format

--- infrastructure/mesh-forwarder.tf
+++ infrastructure/mesh-forwarder.tf
@@ -260,8 +260,10 @@ module "sqs-nems-queue" {
   environment       = var.environment
   owner             = var.owner
   message_retention = 1800
-  kms_master_key_id = module.sns_encryption_key.id
-  enable_sse        = null
+  # kms_master_key_id = module.sns_encryption_key.id
+  enable_sse     = true
+  max_visibility = 60
+  max_message    = 262144
 }
 
 data "aws_iam_policy_document" "sqs_policy_doc" {
