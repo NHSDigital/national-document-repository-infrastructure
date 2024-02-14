40d94f4824ab66d3e28afce386535a247c4fc1d2
abbas-khan10 <abbas.khan10@nhs.net>
2024-02-14 11:52:18 +0000

    fix versions

--- infrastructure/modules/app_config/versions.tf
+++ infrastructure/modules/app_config/versions.tf
@@ -5,7 +5,6 @@ locals {
 resource "aws_appconfig_hosted_configuration_version" "ndr-app-config-profile-version-v1" {
   application_id           = aws_appconfig_application.ndr-app-config-application.id
   configuration_profile_id = aws_appconfig_configuration_profile.ndr-app-config-profile.configuration_profile_id
-  description              = "version-${sha256(file("${local.config_path}/v1.json"))}"
   content                  = file("${local.config_path}/v1.json")
   content_type             = "application/json"
 
@@ -17,7 +16,6 @@ resource "aws_appconfig_hosted_configuration_version" "ndr-app-config-profile-ve
 resource "aws_appconfig_hosted_configuration_version" "ndr-app-config-profile-version-v2" {
   application_id           = aws_appconfig_application.ndr-app-config-application.id
   configuration_profile_id = aws_appconfig_configuration_profile.ndr-app-config-profile.configuration_profile_id
-  description              = "version-${sha256(file("${local.config_path}/v2.json"))}"
   content                  = file("${local.config_path}/v2.json")
   content_type             = "application/json"
 
