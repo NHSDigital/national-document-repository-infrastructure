d4769dcc2c13dead4bca518658648862338e0c0f
Rio Knightley <rio.knightley2@nhs.net>
2024-02-28 14:17:34 +0000

    Add virus scan lambda

---
+++ infrastructure/lambda-virus-scan-result.tf
@@ -0,0 +1,91 @@
+module "virus_scan_result_gateway" {
+  # Gateway Variables
+  source              = "./modules/gateway"
+  api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  parent_id           = aws_api_gateway_rest_api.ndr_doc_store_api.root_resource_id
+  http_method         = "POST"
+  authorization       = "CUSTOM"
+  gateway_path        = "VirusScanResult"
+  authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
+  require_credentials = true
+  origin              = "'https://${terraform.workspace}.${var.domain}'"
+
+  # Lambda Variables
+  api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
+  owner             = var.owner
+  environment       = var.environment
+
+  depends_on = [
+    aws_api_gateway_rest_api.ndr_doc_store_api,
+  ]
+}
+
+module "virus_scan_result_alarm" {
+  source               = "./modules/lambda_alarms"
+  lambda_function_name = module.virus_scan_result_lambda.function_name
+  lambda_timeout       = modulevirus_scan_result_lambda.timeout
+  lambda_name          = "virus_scan_result_handler"
+  namespace            = "AWS/Lambda"
+  alarm_actions        = [module.virus_scan_result_alarm_topic.arn]
+  ok_actions           = [module.virus_scan_result_alarm_topic.arn]
+  depends_on           = [module.virus_scan_result_lambda, module.virus_scan_result_alarm_topic]
+}
+
+
+module "virus_scan_result_alarm_topic" {
+  source                = "./modules/sns"
+  sns_encryption_key_id = module.sns_encryption_key.id
+  current_account_id    = data.aws_caller_identity.current.account_id
+  topic_name            = "virus_scan_result_alarm-topic"
+  topic_protocol        = "lambda"
+  topic_endpoint        = module.virus_scan_result_lambda.endpoint
+  depends_on            = [module.sns_encryption_key]
+  delivery_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudwatch.amazonaws.com"
+        },
+        "Action" : [
+          "SNS:Publish",
+        ],
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "arn:aws:cloudwatch:eu-west-2:${data.aws_caller_identity.current.account_id}:alarm:*"
+          }
+        }
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+module "virus_scan_result_lambda" {
+  source  = "./modules/lambda"
+  name    = "CreateDocRefLambda"
+  handler = "handlers.create_document_reference_handler.lambda_handler"
+  iam_role_policies = [
+    module.ndr-bulk-staging-store.s3_object_access_policy,
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+    "arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy",
+    module.ndr-app-config.app_config_policy_arn
+  ]
+  rest_api_id       = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  resource_id       = module.virus_scan_result_gateway.gateway_resource_id
+  http_method       = "POST"
+  api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
+  lambda_environment_variables = {
+    APPCONFIG_APPLICATION        = module.ndr-app-config.app_config_application_id
+    APPCONFIG_ENVIRONMENT        = module.ndr-app-config.app_config_environment_id
+    APPCONFIG_CONFIGURATION      = module.ndr-app-config.app_config_configuration_profile_id
+    STAGING_STORE_BUCKET_NAME = "${terraform.workspace}-${var.staging_store_bucket_name}"
+    WORKSPACE                    = terraform.workspace
+  }
+  depends_on = [
+    module.ndr-bulk-staging-store,
+    module.virus_scan_result_gateway,
+    module.ndr-app-config
+  ]
+}
