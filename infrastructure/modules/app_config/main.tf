6687df95492ea82e63bc03627549af0af3ddcfbe
Joe Fong <joe.fong1@nhs.net>
2024-05-21 13:59:37 +0100

    revert temp change for ndrc

--- infrastructure/modules/app_config/main.tf
+++ infrastructure/modules/app_config/main.tf
@@ -1,7 +1,7 @@
 locals {
-  is_sandbox_or_test = contains(["ndra", "ndrb", "ndrd", "ndr-test"], terraform.workspace)
-  is_preprod         = contains(["pre-prod", "ndrc"], terraform.workspace)
-  current_config_path = (local.is_sandbox_or_test ? "${path.module}/configurations/2024-02-21-dev.json"
+  is_sandbox_dev_or_test = contains(["ndra", "ndrb", "ndrc", "ndrd", "ndr-dev", "ndr-test"], terraform.workspace)
+  is_preprod             = contains(["pre-prod"], terraform.workspace)
+  current_config_path = (local.is_sandbox_dev_or_test ? "${path.module}/configurations/2024-02-21-dev.json"
     : local.is_preprod ? "${path.module}/configurations/2024-05-21-preprod.json"
   : "${path.module}/configurations/2024-02-21-prod.json")
 }
