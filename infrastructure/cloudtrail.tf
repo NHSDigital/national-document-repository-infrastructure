b3a679b277b6448eee81ecb2f80f14209029143f
NogaNHS <noga.sasson1@nhs.net>
2024-06-26 11:46:57 +0100

    [PRMDR-447] cloudtrail add ${terraform.workspace}

--- infrastructure/cloudtrail.tf
+++ infrastructure/cloudtrail.tf
@@ -1,14 +1,14 @@
 resource "aws_cloudtrail" "cloudtrail_log" {
   depends_on = [aws_s3_bucket_policy.cloudtrail_bucket_policy]
 
-  name                          = "example"
+  name                          = "${terraform.workspace}-cloudtrail-audit"
   s3_bucket_name                = aws_s3_bucket.cloudtrail_logs.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
 
 resource "aws_s3_bucket" "cloudtrail_logs" {
-  bucket        = "tf-cloudtrail-logs"
+  bucket        = "${terraform.workspace}-tf-cloudtrail-logs"
   force_destroy = true
 }
 
