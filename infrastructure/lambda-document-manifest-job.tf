21c7f4a3f6cc76019723404bb3933c1358e4e677
abbas-khan10 <127417949+abbas-khan10@users.noreply.github.com>
2024-07-09 16:59:55 +0100

    PRMDR-565 - Download manifest polling and gateway methods refactor (#182)
    * add lambdas and dynamo stream trigger, update gateway methods
    
    * fix feature flag dependency
    
    * add env vars for dynamo
    
    * update document manifest lambda name
    
    * Change manifest lambda and add dynamo stream filtering
    
    * fix dynamo filtering
    
    * update filter to only trigger on insert
    
    * format
    
    * fix filter
    
    * fix filter
    
    * fix filter formatting
    
    * [PRMDR-565] run format
    
    * [PRMDR-565] PR changes
    
    ---------
    
    Co-authored-by: NogaNHS <noga.sasson1@nhs.net>

--- infrastructure/lambda-document-manifest-job.tf
+++ infrastructure/lambda-document-manifest-job.tf
@@ -1,9 +1,9 @@
-module "document-manifest-by-nhs-gateway" {
+module "document-manifest-job-gateway" {
   # Gateway Variables
   source              = "./modules/gateway"
   api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
   parent_id           = aws_api_gateway_rest_api.ndr_doc_store_api.root_resource_id
-  http_method         = "GET"
+  http_methods        = ["GET", "POST"]
   authorization       = "CUSTOM"
   gateway_path        = "DocumentManifest"
   authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
@@ -22,13 +22,13 @@ module "document-manifest-by-nhs-gateway" {
 
 module "document_manifest_alarm" {
   source               = "./modules/lambda_alarms"
-  lambda_function_name = module.document-manifest-by-nhs-number-lambda.function_name
-  lambda_timeout       = module.document-manifest-by-nhs-number-lambda.timeout
+  lambda_function_name = module.document-manifest-job-lambda.function_name
+  lambda_timeout       = module.document-manifest-job-lambda.timeout
   lambda_name          = "create_document_manifest_handler"
   namespace            = "AWS/Lambda"
   alarm_actions        = [module.document_manifest_alarm_topic.arn]
   ok_actions           = [module.document_manifest_alarm_topic.arn]
-  depends_on           = [module.document-manifest-by-nhs-number-lambda, module.document_manifest_alarm_topic]
+  depends_on           = [module.document-manifest-job-lambda, module.document_manifest_alarm_topic]
 }
 
 
@@ -38,7 +38,7 @@ module "document_manifest_alarm_topic" {
   current_account_id    = data.aws_caller_identity.current.account_id
   topic_name            = "create_doc_manifest-alarms-topic"
   topic_protocol        = "lambda"
-  topic_endpoint        = module.document-manifest-by-nhs-number-lambda.endpoint
+  topic_endpoint        = module.document-manifest-job-lambda.lambda_arn
   depends_on            = [module.sns_encryption_key]
   delivery_policy = jsonencode({
     "Version" : "2012-10-17",
@@ -62,17 +62,14 @@ module "document_manifest_alarm_topic" {
   })
 }
 
-module "document-manifest-by-nhs-number-lambda" {
-  source                   = "./modules/lambda"
-  name                     = "DocumentManifestByNHSNumberLambda"
-  handler                  = "handlers.document_manifest_by_nhs_number_handler.lambda_handler"
-  lambda_timeout           = 900
-  lambda_ephemeral_storage = 512
+module "document-manifest-job-lambda" {
+  source         = "./modules/lambda"
+  name           = "DocumentManifestJobLambda"
+  handler        = "handlers.document_manifest_job_handler.lambda_handler"
+  lambda_timeout = 900
   iam_role_policies = [
     module.document_reference_dynamodb_table.dynamodb_policy,
-    module.ndr-document-store.s3_object_access_policy,
     module.lloyd_george_reference_dynamodb_table.dynamodb_policy,
-    module.ndr-lloyd-george-store.s3_object_access_policy,
     module.zip_store_reference_dynamodb_table.dynamodb_policy,
     module.ndr-zip-request-store.s3_object_access_policy,
     "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
@@ -80,16 +77,14 @@ module "document-manifest-by-nhs-number-lambda" {
     module.ndr-app-config.app_config_policy_arn
   ]
   rest_api_id       = aws_api_gateway_rest_api.ndr_doc_store_api.id
-  resource_id       = module.document-manifest-by-nhs-gateway.gateway_resource_id
-  http_method       = "GET"
+  resource_id       = module.document-manifest-job-gateway.gateway_resource_id
+  http_methods      = ["GET", "POST"]
   api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
   lambda_environment_variables = {
     APPCONFIG_APPLICATION        = module.ndr-app-config.app_config_application_id
     APPCONFIG_ENVIRONMENT        = module.ndr-app-config.app_config_environment_id
     APPCONFIG_CONFIGURATION      = module.ndr-app-config.app_config_configuration_profile_id
-    DOCUMENT_STORE_BUCKET_NAME   = "${terraform.workspace}-${var.docstore_bucket_name}"
     DOCUMENT_STORE_DYNAMODB_NAME = "${terraform.workspace}_${var.docstore_dynamodb_table_name}"
-    LLOYD_GEORGE_BUCKET_NAME     = "${terraform.workspace}-${var.lloyd_george_bucket_name}"
     LLOYD_GEORGE_DYNAMODB_NAME   = "${terraform.workspace}_${var.lloyd_george_dynamodb_table_name}"
     ZIPPED_STORE_BUCKET_NAME     = "${terraform.workspace}-${var.zip_store_bucket_name}"
     ZIPPED_STORE_DYNAMODB_NAME   = "${terraform.workspace}_${var.zip_store_dynamodb_table_name}"
@@ -99,14 +94,18 @@ module "document-manifest-by-nhs-number-lambda" {
   }
   depends_on = [
     aws_api_gateway_rest_api.ndr_doc_store_api,
-    module.document-manifest-by-nhs-gateway,
+    module.document-manifest-job-gateway,
     aws_iam_policy.lambda_audit_splunk_sqs_queue_send_policy[0],
-    module.ndr-app-config
+    module.ndr-app-config,
+    module.lloyd_george_reference_dynamodb_table,
+    module.document_reference_dynamodb_table,
+    module.zip_store_reference_dynamodb_table,
+    module.ndr-zip-request-store
   ]
 }
 
 resource "aws_iam_role_policy_attachment" "policy_manifest_lambda" {
   count      = local.is_sandbox ? 0 : 1
-  role       = module.document-manifest-by-nhs-number-lambda.lambda_execution_role_name
+  role       = module.document-manifest-job-lambda.lambda_execution_role_name
   policy_arn = try(aws_iam_policy.lambda_audit_splunk_sqs_queue_send_policy[0].arn, null)
 }
