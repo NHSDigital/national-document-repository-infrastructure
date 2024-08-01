9eec0f03add7c2939c604ca31da4595e977c7214
abbas-khan10 <abbas.khan10@nhs.net>
2024-08-01 11:50:18 +0100

    create ndra as prod

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -211,8 +211,8 @@ variable "message_destination" {
 variable "cloudwatch_alarm_evaluation_periods" {}
 
 locals {
-  is_sandbox         = contains(["ndra", "ndrb", "ndrc", "ndrd"], terraform.workspace)
-  is_production      = contains(["pre-prod", "prod"], terraform.workspace)
+  is_sandbox         = contains(["ndrb", "ndrc", "ndrd"], terraform.workspace)
+  is_production      = contains(["ndra", "pre-prod", "prod"], terraform.workspace)
   is_force_destroy   = contains(["ndr-dev", "ndra", "ndrb", "ndrc", "ndrd", "ndr-test"], terraform.workspace)
   is_sandbox_or_test = contains(["ndra", "ndrb", "ndrc", "ndrd", "ndr-test"], terraform.workspace)
 
