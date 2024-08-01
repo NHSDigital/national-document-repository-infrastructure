010f3478ea6ed0eeb9836b9a4b16fd23407c20fd
abbas-khan10 <abbas.khan10@nhs.net>
2024-08-01 12:24:04 +0100

    fix

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -211,7 +211,7 @@ variable "message_destination" {
 variable "cloudwatch_alarm_evaluation_periods" {}
 
 locals {
-  is_sandbox         = contains(["ndrb", "ndrc", "ndrd"], terraform.workspace)
+  is_sandbox         = contains(["ndra","ndrb", "ndrc", "ndrd"], terraform.workspace)
   is_production      = contains(["ndra", "pre-prod", "prod"], terraform.workspace)
   is_force_destroy   = contains(["ndr-dev", "ndra", "ndrb", "ndrc", "ndrd", "ndr-test"], terraform.workspace)
   is_sandbox_or_test = contains(["ndra", "ndrb", "ndrc", "ndrd", "ndr-test"], terraform.workspace)
