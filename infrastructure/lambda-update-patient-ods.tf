2e2481984ede8f157f0d72b65ea93ee77f27f46b
Steph Torres <stephane.torres1@nhs.net>
2024-08-15 15:38:05 +0100

    [PRMP-763] add alarm topic to update-patient-ods-lambda

--- infrastructure/lambda-update-patient-ods.tf
+++ infrastructure/lambda-update-patient-ods.tf
@@ -17,15 +17,57 @@ module "update-patient-ods-lambda" {
         APPCONFIG_CONFIGURATION    = module.ndr-app-config.app_config_configuration_profile_id
         WORKSPACE                  = terraform.workspace
         LLOYD_GEORGE_DYNAMODB_NAME = "${terraform.workspace}_${var.lloyd_george_dynamodb_table_name}"
-        //PDS_FHIR_IS_STUBBED        = local.is_sandbox
+        PDS_FHIR_IS_STUBBED        = local.is_sandbox
     }
     is_gateway_integration_needed = false
     is_invoked_from_gateway       = false
     memory_size                   = 512
-    //lambda_timeout                = 45
+    lambda_timeout                = 900
 
     depends_on = [
         module.ndr-lloyd_george_reference_dynamodb_table
         module.ndr-app-config
     ]
+}
+
+module "update-patient-ods-alarm" {
+    source               = "./modules/lambda_alarms"
+    lambda_function_name = module.update-patient-ods-lambda.function_name
+    lambda_timeout       = module.update-patient-ods-lambda.lambda_timeout
+    lambda_name          = "update_patient_ods_handler"
+    namespace            = "AWS/Lambda"
+    alarm_actions        = [module.update-patient-ods-topic.arn]
+    ok_actions           = [module.update-patient-ods-topic.arn]
+    depends_on           = [module.update-patient-ods-lambda, module.update-patient-ods-topic]
+}
+
+module "update-patient-alarm-topic" {
+    source = "./modules/sns"
+      sns_encryption_key_id = module.sns_encryption_key.id
+  current_account_id    = data.aws_caller_identity.current.account_id
+  topic_name            = "update-patient-ods--topic"
+  topic_protocol        = "lambda"
+  topic_endpoint        = module.update-patient-ods-lambda.lambda_arn
+  delivery_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudwatch.amazonaws.com"
+        },
+        "Action" : [
+          "SNS:Publish",
+        ],
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "arn:aws:cloudwatch:eu-west-2:${data.aws_caller_identity.current.account_id}:alarm:*"
+          }
+        }
+        "Resource" : "*"
+      }
+    ]
+  })
+
+  depends_on = [module.update-patient-ods-lambda, module.sns_encryption_key]
 }
