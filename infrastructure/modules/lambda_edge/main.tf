aa52ef8d2724920c498ef93925cc4060f962ae20
Rio Knightley <rio.knightley2@nhs.net>
2024-08-15 12:43:37 +0100

    Disable lambda insights extension

--- infrastructure/modules/lambda_edge/main.tf
+++ infrastructure/modules/lambda_edge/main.tf
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "lambda" {
   memory_size                    = var.memory_size
   reserved_concurrent_executions = var.reserved_concurrent_executions
   layers = [
-    "arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension:38",
+    # "arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension:38",
     "arn:aws:lambda:eu-west-2:282860088358:layer:AWS-AppConfig-Extension:81"
   ]
   ephemeral_storage {
