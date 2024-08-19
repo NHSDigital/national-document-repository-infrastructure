85c88e2fa525fd0d731a3bf84ddcd6d317987074
Rio Knightley <rio.knightley2@nhs.net>
2024-08-19 16:30:35 +0100

    Restore lg table name

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -54,6 +54,12 @@ variable "docstore_dynamodb_table_name" {
   default     = "DocumentReferenceMetadata"
 }
 
+variable "lloyd_george_dynamodb_table_name" {
+  type        = string
+  description = "The name of dynamodb table to store the metadata of Lloyd George documents"
+  default     = "LloydGeorgeReferenceMetadata"
+}
+
 variable "cloudfront_edge_table_name" {
   type        = string
   description = "The name of dynamodb table to store the metadata of Lloyd George documents"
