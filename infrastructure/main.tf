e504456c20009a239feba6d95597bdff902110ec
Rio Knightley <rio.knightley2@nhs.net>
2024-09-04 14:50:19 +0100

    Remove unnecessary provider

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,10 +18,6 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
 data "aws_caller_identity" "current" {
 }
 
--- infrastructure/modules/lambda_edge/main.tf
+++ infrastructure/modules/lambda_edge/main.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=5.11"
+      version = "~> 5.0"
     }
   }
 }
 
 resource "aws_lambda_function" "lambda" {
-  provider = aws.us_east_1
+  provider = aws
   publish  = true
 
   filename                       = data.archive_file.lambda.output_path
