24c943a4e357e4585c506623179f85d4db3dd3e7
abbas-khan10 <abbas.khan10@nhs.net>
2024-09-09 14:51:56 +0100

    PRMDR-0000 - Revert prod changes to disable individual user upload

--- infrastructure/lambda-virus-scan-result.tf
+++ infrastructure/lambda-virus-scan-result.tf
@@ -76,12 +76,10 @@ module "virus_scan_result_lambda" {
     module.document_reference_dynamodb_table.dynamodb_policy,
     module.lloyd_george_reference_dynamodb_table.dynamodb_policy,
   ]
-  rest_api_id                   = local.is_production ? null : aws_api_gateway_rest_api.ndr_doc_store_api.id
-  resource_id                   = local.is_production ? null : module.virus_scan_result_gateway.gateway_resource_id
-  api_execution_arn             = local.is_production ? null : aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
-  is_gateway_integration_needed = local.is_production ? false : true
-  is_invoked_from_gateway       = local.is_production ? false : true
-  http_methods                  = ["POST"]
+  rest_api_id       = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  resource_id       = module.virus_scan_result_gateway.gateway_resource_id
+  http_methods      = ["POST"]
+  api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
   lambda_environment_variables = {
     APPCONFIG_APPLICATION        = module.ndr-app-config.app_config_application_id
     APPCONFIG_ENVIRONMENT        = module.ndr-app-config.app_config_environment_id
