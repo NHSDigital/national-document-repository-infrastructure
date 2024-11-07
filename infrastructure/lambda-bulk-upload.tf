5bf27851d8c2ad4b7663abd894dd338251f46d6e
Jack Sutton <jack.sutton@madetech.com>
2024-11-07 15:01:10 +0000

    adds new env variable to bulk upload lambda

--- infrastructure/lambda-bulk-upload.tf
+++ infrastructure/lambda-bulk-upload.tf
@@ -29,6 +29,7 @@ module "bulk-upload-lambda" {
     METADATA_SQS_QUEUE_URL     = module.sqs-lg-bulk-upload-metadata-queue.sqs_url
     INVALID_SQS_QUEUE_URL      = module.sqs-lg-bulk-upload-invalid-queue.sqs_url
     PDS_FHIR_IS_STUBBED        = local.is_sandbox
+    METADATA_NRL_SQS_URL       = module.sqs-nrl-queue.sqs_url
   }
 
   is_gateway_integration_needed  = false
