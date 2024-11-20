1fbcfd45cdf7b533fe09aa78f92daee211ec8777
abid-nhs <abdur.abid1@nhs.net>
2024-11-20 15:41:05 +0000

    PRMP-1221 remove the combined polcies for s2 and dynamodb

--- infrastructure/lambda-bulk-upload.tf
+++ infrastructure/lambda-bulk-upload.tf
@@ -8,8 +8,10 @@ module "bulk-upload-lambda" {
     module.ndr-app-config.app_config_policy_arn,
     aws_iam_policy.ssm_access_policy.arn,
     aws_iam_policy.lambda_sqs_combined_policy.arn,
-    aws_iam_policy.lambda_s3_combined_policy.arn,
-    aws_iam_policy.lambda_dynamodb_combined_policy.arn
+    module.ndr-bulk-staging-store.s3_object_access_policy,
+    module.ndr-lloyd-george-store.s3_object_access_policy,
+    module.lloyd_george_reference_dynamodb_table.dynamodb_policy,
+    module.bulk_upload_report_dynamodb_table.dynamodb_policy,
   ]
   rest_api_id       = null
   api_execution_arn = null
--- infrastructure/policies.tf
+++ infrastructure/policies.tf
@@ -26,35 +26,8 @@ data "aws_iam_policy_document" "combined_sqs_policies" {
   ]
 }
 
-data "aws_iam_policy_document" "combined_s3_policies" {
-  source_policy_documents = [
-    module.ndr-bulk-staging-store.s3_object_access_policy_json,
-    module.ndr-lloyd-george-store.s3_object_access_policy_json
-  ]
-}
-
-data "aws_iam_policy_document" "combined_dynamodb_policies" {
-  source_policy_documents = [
-    module.lloyd_george_reference_dynamodb_table.dynamodb_policy_json,
-    module.bulk_upload_report_dynamodb_table.dynamodb_policy_json
-  ]
-}
-
 resource "aws_iam_policy" "lambda_sqs_combined_policy" {
   name        = "${terraform.workspace}-lambda-sqs-combined-policy"
   description = "Combined SQS policies for Lambda"
   policy      = data.aws_iam_policy_document.combined_sqs_policies.json
-}
-
-
-resource "aws_iam_policy" "lambda_s3_combined_policy" {
-  name        = "${terraform.workspace}-lambda-s3-combined-policy"
-  description = "Combined S3 policies for Lambda"
-  policy      = data.aws_iam_policy_document.combined_s3_policies.json
-}
-
-resource "aws_iam_policy" "lambda_dynamodb_combined_policy" {
-  name        = "${terraform.workspace}-lambda-dynamodb-combined-policy"
-  description = "Combined DynamoDB policies for Lambda"
-  policy      = data.aws_iam_policy_document.combined_dynamodb_policies.json
 }
