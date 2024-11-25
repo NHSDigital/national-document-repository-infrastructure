0d69a07e3245673c777e4feab1043e4181f293a4
abid-nhs <abdur.abid1@nhs.net>
2024-11-25 11:53:52 +0000

    PRMP-1221 - fix invalid index error

--- infrastructure/modules/sqs/main.tf
+++ infrastructure/modules/sqs/main.tf
@@ -83,10 +83,10 @@ data "aws_iam_policy_document" "sqs_read_policy" {
       "sqs:GetQueueAttributes",
       "sqs:GetQueueUrl"
     ]
-    resources = [
-      aws_sqs_queue.sqs_queue.arn,
-      aws_sqs_queue.queue_deadletter[0].arn
-    ]
+    resources = concat(
+      [aws_sqs_queue.sqs_queue.arn],
+      var.enable_dlq ? [aws_sqs_queue.queue_deadletter[0].arn] : []
+    )
   }
 }
 
@@ -97,9 +97,9 @@ data "aws_iam_policy_document" "sqs_write_policy" {
       "sqs:SendMessage",
       "sqs:DeleteMessage"
     ]
-    resources = [
-      aws_sqs_queue.sqs_queue.arn,
-      aws_sqs_queue.queue_deadletter[0].arn
-    ]
+    resources = concat(
+      [aws_sqs_queue.sqs_queue.arn],
+      var.enable_dlq ? [aws_sqs_queue.queue_deadletter[0].arn] : []
+    )
   }
 }
