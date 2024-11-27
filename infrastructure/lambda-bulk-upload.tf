19128efd344cfcc41fd2998b5fef23572a76b412
abid-nhs <abdur.abid1@nhs.net>
2024-11-27 12:53:14 +0000

    PRMP-1221 remove all additional_policy_arns and pass them in docs

--- infrastructure/lambda-bulk-upload.tf
+++ infrastructure/lambda-bulk-upload.tf
@@ -16,7 +16,7 @@ module "bulk-upload-lambda" {
     module.sqs-lg-bulk-upload-metadata-queue.sqs_read_policy_document,
     module.sqs-lg-bulk-upload-metadata-queue.sqs_write_policy_document,
     module.sqs-lg-bulk-upload-invalid-queue.sqs_read_policy_document,
-    module.sqs-lg-bulk-upload-invalid-queue.sqs_write_policy_document
+    module.sqs-lg-bulk-upload-invalid-queue.sqs_write_policy_document,
 
     aws_iam_policy.ssm_access_policy.arn,
     module.ndr-app-config.app_config_policy_arn
