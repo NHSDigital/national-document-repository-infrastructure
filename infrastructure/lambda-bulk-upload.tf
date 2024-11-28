65d10a180ef349415ab5ae53885059fd49966aef
abid-nhs <abdur.abid1@nhs.net>
2024-11-28 09:20:35 +0000

    PRMP-1221 undo a change causing errors

--- infrastructure/lambda-bulk-upload.tf
+++ infrastructure/lambda-bulk-upload.tf
@@ -17,7 +17,8 @@ module "bulk-upload-lambda" {
     module.sqs-lg-bulk-upload-metadata-queue.sqs_write_policy_document,
     module.sqs-lg-bulk-upload-invalid-queue.sqs_read_policy_document,
     module.sqs-lg-bulk-upload-invalid-queue.sqs_write_policy_document,
-    ssm_policy_test,
+
+    aws_iam_policy.ssm_access_policy.policy,
     module.ndr-app-config.app_config_policy
   ]
   rest_api_id       = null
