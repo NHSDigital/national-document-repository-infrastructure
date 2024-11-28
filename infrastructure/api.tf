a705cff8740cf515bd729f56f87b989badc590aa
NogaNHS <noga.sasson1@nhs.net>
2024-11-28 14:35:10 +0000

    [PRMP-1286] add lambda to api depends_on

--- infrastructure/api.tf
+++ infrastructure/api.tf
@@ -103,7 +103,9 @@ resource "aws_api_gateway_deployment" "ndr_api_deploy" {
     module.upload_confirm_result_gateway,
     module.upload_confirm_result_lambda,
     module.virus_scan_result_gateway,
-    module.virus_scan_result_lambda
+    module.virus_scan_result_lambda,
+    module.get-doc-nrl-gateway,
+    module.get-doc-nrl-lambda
   ]
 
   lifecycle {
