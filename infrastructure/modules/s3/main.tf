4b59c5ac25c82b7af6c5a3884292392e9be0c104
abid-nhs <abdur.abid1@nhs.net>
2024-11-28 11:12:34 +0000

    PRMP-1221 Syntax errors in policy.

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -133,9 +133,9 @@ data "aws_iam_policy_document" "s3_read_policy" {
   statement {
     actions = ["s3:GetObject", "s3:ListBucket"]
     resources = [
-      # aws_s3_bucket.bucket.arn,
-      # "${aws_s3_bucket.bucket.arn}/*"
-      "*"
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*"
+      # "*"
     ]
   }
 }
@@ -145,7 +145,6 @@ data "aws_iam_policy_document" "s3_write_policy" {
     actions = ["s3:PutObject"]
     resources = [
       "${aws_s3_bucket.bucket.arn}/*"
-      # "*"
     ]
   }
 }
