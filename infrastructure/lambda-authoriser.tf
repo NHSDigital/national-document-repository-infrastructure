8cc385ec093d1f972ca1ac204c6bb2732ce262e2
abid-nhs <abdur.abid1@nhs.net>
2024-11-28 14:02:34 +0000

    PRMP-1221 bring back some of the commented out policies

--- infrastructure/lambda-authoriser.tf
+++ infrastructure/lambda-authoriser.tf
@@ -3,10 +3,10 @@ module "authoriser-lambda" {
   name    = "AuthoriserLambda"
   handler = "handlers.authoriser_handler.lambda_handler"
   iam_role_policy_documents = [
-    # aws_iam_policy.ssm_policy_authoriser.policy,
+    aws_iam_policy.ssm_policy_authoriser.policy,
     module.auth_session_dynamodb_table.dynamodb_read_policy_document,
     module.auth_session_dynamodb_table.dynamodb_write_policy_document,
-    # module.ndr-app-config.app_config_policy
+    module.ndr-app-config.app_config_policy
   ]
   rest_api_id       = aws_api_gateway_rest_api.ndr_doc_store_api.id
   api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
