e6f31e6bdf0d99a3073d7c9d9851305133fab48f
NogaNHS <noga.sasson1@nhs.net>
2024-12-04 14:07:57 +0000

    [PRMP-1286] change to resource id in lambda

--- infrastructure/lambda-get-document-nrl.tf
+++ infrastructure/lambda-get-document-nrl.tf
@@ -22,7 +22,7 @@ module "get-doc-nrl-lambda" {
     module.ndr-app-config.app_config_policy_arn
   ]
   rest_api_id       = aws_api_gateway_rest_api.ndr_doc_store_api.id
-  resource_id       = module.create-doc-ref-gateway.gateway_resource_id
+  resource_id       = aws_api_gateway_resource.document_ref_get.id
   http_methods      = ["GET"]
   api_execution_arn = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
   lambda_environment_variables = {
