11ad7e9fa4c851b065558e4ab7af8e9692dddc9f
NogaNHS <noga.sasson1@nhs.net>
2024-12-10 10:27:27 +0000

    [PRMP-1286] change aws_api_gateway_api_key name

--- infrastructure/apim-nrl.tf
+++ infrastructure/apim-nrl.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_usage_plan" "apim" {
-  name = "${terraform.workspace}_apim_usage_plan"
+  name = "${terraform.workspace}_apim-usage-plan"
   api_stages {
     api_id = aws_api_gateway_rest_api.ndr_doc_store_api.id
     stage  = aws_api_gateway_stage.ndr_api.stage_name
@@ -7,7 +7,7 @@ resource "aws_api_gateway_usage_plan" "apim" {
 }
 
 resource "aws_api_gateway_api_key" "apim" {
-  name = "${terraform.workspace}_apim_api_key"
+  name = "${terraform.workspace}_apim-api-key"
 }
 
 resource "aws_api_gateway_usage_plan_key" "apim" {
