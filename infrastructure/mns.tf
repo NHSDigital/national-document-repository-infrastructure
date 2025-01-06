423390e86c2941e5b4e5815dbf374b00a0dbe6eb
NogaNHS <noga.sasson1@nhs.net>
2025-01-06 10:37:35 +0000

    [PRMP-1401] Adding a count on mns queue

--- infrastructure/mns.tf
+++ infrastructure/mns.tf
@@ -17,6 +17,7 @@ module "mns_encryption_key" {
 }
 
 module "sqs-mns-notification-queue" {
+  count             = local.is_sandbox ? 0 : 1
   source            = "./modules/sqs"
   name              = "mns-notification-queue"
   max_size_message  = 256 * 1024        # allow message size up to 256 KB
@@ -26,11 +27,13 @@ module "sqs-mns-notification-queue" {
   max_visibility    = 1020
   delay             = 60
   enable_sse        = null
-  kms_master_key_id = module.mns_encryption_key.id
+  kms_master_key_id = module.mns_encryption_key[0].id
 }
 
 resource "aws_sqs_queue_policy" "mns_sqs_access" {
-  queue_url = module.sqs-mns-notification-queue.sqs_url
+  count = local.is_sandbox ? 0 : 1
+
+  queue_url = module.sqs-mns-notification-queue[0].sqs_url
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -41,7 +44,7 @@ resource "aws_sqs_queue_policy" "mns_sqs_access" {
           AWS = data.aws_ssm_parameter.mns_lambda_role.value
         },
         Action   = "SQS:SendMessage",
-        Resource = module.sqs-mns-notification-queue.sqs_arn
+        Resource = module.sqs-mns-notification-queue[0].sqs_arn
       }
     ]
   })
