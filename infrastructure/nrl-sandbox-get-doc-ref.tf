e2575d086586d8649ea4c7cf2af1d6c0ffc51b2e
Steph Torres <stephane.torres1@nhs.net>
2025-01-24 10:00:22 +0000

    [PRMP-1302] address PR comments

--- infrastructure/nrl-sandbox-get-doc-ref.tf
+++ infrastructure/nrl-sandbox-get-doc-ref.tf
@@ -18,7 +18,7 @@ resource "aws_api_gateway_resource" "sandbox_get_document_reference" {
   path_part   = "DocumentReference"
 }
 
-resource "aws_api_gateway_resource" "sandbox_get_document_reference_path_parametre" {
+resource "aws_api_gateway_resource" "sandbox_get_document_reference_path_parameter" {
   rest_api_id = aws_api_gateway_rest_api.ndr_doc_store_api.id
   parent_id   = aws_api_gateway_resource.sandbox_get_document_reference.id
   path_part   = "{id}"
@@ -36,7 +36,7 @@ resource "aws_api_gateway_method" "sandbox_get_document_reference" {
 }
 
 
-resource "aws_api_gateway_integration" "get_document_reference_mock_integration" {
+resource "aws_api_gateway_integration" "get_document_reference_mock" {
   rest_api_id = aws_api_gateway_rest_api.ndr_doc_store_api.id
   resource_id = aws_api_gateway_resource.sandbox_get_document_reference_path_parametre.id
   type        = "MOCK"
