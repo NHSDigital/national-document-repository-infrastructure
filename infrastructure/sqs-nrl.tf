1c4032a799d379e6ac7826aad123b1400ef4124c
NogaNHS <noga.sasson1@nhs.net>
2025-01-28 13:50:27 +0000

    [PRMP-1469] add alarm to dlq nrl

--- infrastructure/sqs-nrl.tf
+++ infrastructure/sqs-nrl.tf
@@ -10,3 +10,19 @@ module "sqs-nrl-queue" {
   enable_deduplication = true
   enable_dlq           = true
 }
+
+resource "aws_cloudwatch_metric_alarm" "nrl_dlq_new_messages_alarm" {
+  alarm_name          = "NRL_DLQ_MESSAGES"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = "60"
+  statistic           = "Sum"
+  threshold           = "0"
+  alarm_description   = "Alarm when there are new messages in the nrl dlq queue"
+
+  dimensions = {
+    QueueName = module.sqs-nrl-queue.dlq_name
+  }
+}
