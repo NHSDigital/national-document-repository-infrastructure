a7e5efbf3ec6b9cc392c654a6f7547a7e3bc362e
Jack Sutton <jack.sutton@madetech.com>
2025-02-07 11:05:16 +0000

    Formatting

--- infrastructure/sqs-stitching-dlq.tf
+++ infrastructure/sqs-stitching-dlq.tf
@@ -1,10 +1,10 @@
 module "sqs-stitching-queue-deadletter" {
-  source               = "./modules/sqs"
-  name                 = "stitching-queue-dlq"
-  environment          = var.environment
-  owner                = var.owner
-  message_retention    = 1800
-  enable_sse           = true
-  max_visibility       = 601
-  enable_dlq           = true
+  source            = "./modules/sqs"
+  name              = "stitching-queue-dlq"
+  environment       = var.environment
+  owner             = var.owner
+  message_retention = 1800
+  enable_sse        = true
+  max_visibility    = 601
+  enable_dlq        = true
 }
