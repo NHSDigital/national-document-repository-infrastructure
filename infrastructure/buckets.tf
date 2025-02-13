85024ba2de3783c7210ccfe3720721ff3ade858b
Abbas Khan <abbas.khan10@nhs.net>
2025-02-13 15:31:16 +0000

    refactor policy

--- infrastructure/buckets.tf
+++ infrastructure/buckets.tf
@@ -198,12 +198,6 @@ data "aws_iam_policy_document" "access_logs" {
       type        = "Service"
       identifiers = ["logging.s3.amazonaws.com"]
     }
-
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceAccount"
-      values   = [data.aws_caller_identity.current.account_id]
-    }
   }
   statement {
     sid    = "DenyS3AccessLogsPolicy"
