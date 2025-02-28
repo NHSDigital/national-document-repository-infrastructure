04765823979691a16b62ec237412e0ef029ed1c7
abbas-khan10 <127417949+abbas-khan10@users.noreply.github.com>
2025-02-28 16:59:45 +0000

    [PRMP-1594] Revert [PRMP-1512] BulkUploadLambda - On Success changes
    This reverts commit c661865823f0f483a145b18b4cdfadeac12d3e57.

--- infrastructure/lambda-pdf-stitching.tf
+++ infrastructure/lambda-pdf-stitching.tf
@@ -15,9 +15,6 @@ module "pdf-stitching-lambda" {
     module.unstitched_lloyd_george_reference_dynamodb_table.dynamodb_write_policy_document,
     module.unstitched_lloyd_george_reference_dynamodb_table.dynamodb_read_policy_document
   ]
-  lambda_environment_variables = {
-    PDF_STITCHING_SQS_URL = module.sqs-stitching-queue.sqs_url
-  }
   rest_api_id             = null
   api_execution_arn       = null
   is_invoked_from_gateway = false
