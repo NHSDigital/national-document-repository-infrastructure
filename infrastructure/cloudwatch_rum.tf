3c56344f670246e3a81f75fd4cb497be36cacea0
Ollie Beumkes <oliver.beumkes2@nhs.net>
2025-03-14 11:22:13 +0000

    made region and acc id vars for logging

--- infrastructure/cloudwatch_rum.tf
+++ infrastructure/cloudwatch_rum.tf
@@ -65,7 +65,7 @@ resource "aws_cloudwatch_log_resource_policy" "rum_log_policy" {
           "logs:DeleteLogGroup",
           "logs:DescribeLogGroups"
         ],
-        Resource = "arn:aws:logs:eu-west-2:533825906475:log-group:/aws/vendedlogs/RUMService-*"
+        Resource = "arn:aws:logs:${local.current_region}:${local.current_account_id}:log-group:/aws/vendedlogs/RUMService-*"
       }
     ]
   })
