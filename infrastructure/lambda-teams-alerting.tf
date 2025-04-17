c64ffaf0b8c27d6ba990ed12f564a1cf6d7c5329
Steph Torres <stephane.torres1@nhs.net>
2025-04-17 14:31:02 +0100

    create lambda for teams alerting

---
+++ infrastructure/lambda-teams-alerting.tf
@@ -0,0 +1,35 @@
+module "teams-alerting-lambda" {
+  source  = "./modules/lambda"
+  name    = "TeamsAlertingLambda"
+  handler = "handlers.teams_alerting_handler.lambda_handler"
+  iam_role_policy_documents = [
+    aws_iam_policy.ssm_access_policy.policy,
+    module.ndr-app-config.app_config_policy,
+  ]
+  rest_api_id       = null
+  api_execution_arn = null
+  lambda_environment_variables = {
+    APPCONFIG_APPLICATION      = module.ndr-app-config.app_config_application_id
+    APPCONFIG_ENVIRONMENT      = module.ndr-app-config.app_config_environment_id
+    APPCONFIG_CONFIGURATION    = module.ndr-app-config.app_config_configuration_profile_id
+    WORKSPACE                  = terraform.workspace
+  }
+  is_gateway_integration_needed = false
+  is_invoked_from_gateway       = false
+  lambda_timeout                = 900
+}
+
+
+resource "aws_sns_topic_subscription" "teams_alerting" {
+  endpoint  = module.teams-alerting-lambda.lambda_arn
+  protocol  = "lambda"
+  topic_arn = aws_sns_topic.alarm_notifications_topic.arn
+}
+
+resource "aws_lambda_permission" "invoke_with_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = module.teams-alerting-lambda.lambda_arn
+  principal     = "sns.amazonaws.com"
+  source_arn = aws_sns_topic.alarm_notifications_topic.arn
+}
