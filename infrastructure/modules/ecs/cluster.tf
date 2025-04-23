34705acfd426b83fae9b1d75462d5aeb5087a6ee
Mohammad Iqbal <mohammad.iqbal27@nhs.net>
2025-04-23 13:35:41 +0100

    [PRMT-32] - Enable containerInsights in ECS Cluster module

--- infrastructure/modules/ecs/cluster.tf
+++ infrastructure/modules/ecs/cluster.tf
@@ -11,6 +11,11 @@ resource "aws_ecs_cluster" "ndr_ecs_cluster" {
     }
   }
 
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
   tags = {
     Name        = "${terraform.workspace}-ecs"
     Environment = var.environment
@@ -20,35 +25,5 @@ resource "aws_ecs_cluster" "ndr_ecs_cluster" {
 
 resource "aws_cloudwatch_log_group" "ecs_cluster_logs" {
   name              = "${terraform.workspace}-${var.ecs_cluster_name}-logs"
-  retention_in_days = 0
-}
-
-resource "aws_ecs_cluster" "ndr-ecs-fargate-data-collection" {
-  name = "${terraform.workspace}-${var.ecs_cluster_name}"
-
-  configuration {
-    execute_command_configuration {
-      logging = "OVERRIDE"
-      log_configuration {
-        cloud_watch_encryption_enabled = true
-        cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs_cluster_data_collection_logs.name
-      }
-    }
-  }
-
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
-  }
-
-  tags = {
-    Name        = "${terraform.workspace}-${var.ecs_cluster_name}"
-    Environment = var.environment
-    Workspace   = terraform.workspace
-  }
-}
-
-resource "aws_cloudwatch_log_group" "ecs_cluster_data_collection_logs" {
-  name              = "${terraform.workspace}-data-collection-logs"
   retention_in_days = 0
 }
