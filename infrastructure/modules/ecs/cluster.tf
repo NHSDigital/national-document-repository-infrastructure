d4a97710dcb76ae4a3692d1812d0d0c9d18f2ffc
Mohammad Iqbal <mohammad.iqbal27@nhs.net>
2025-04-23 11:51:01 +0100

    [PRMT-32] - Create cluster for data collection containerInsights

--- infrastructure/modules/ecs/cluster.tf
+++ infrastructure/modules/ecs/cluster.tf
@@ -22,3 +22,33 @@ resource "aws_cloudwatch_log_group" "ecs_cluster_logs" {
   name              = "${terraform.workspace}-${var.ecs_cluster_name}-logs"
   retention_in_days = 0
 }
+
+resource "aws_ecs_cluster" "ndr-ecs-fargate-data-collection" {
+  name = "${terraform.workspace}-${var.ecs_cluster_name}"
+
+  configuration {
+    execute_command_configuration {
+      logging = "OVERRIDE"
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs_cluster_data_collection_logs.name
+      }
+    }
+  }
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  tags = {
+    Name        = "${terraform.workspace}-${var.ecs_cluster_name}"
+    Environment = var.environment
+    Workspace   = terraform.workspace
+  }
+}
+
+resource "aws_cloudwatch_log_group" "ecs_cluster_data_collection_logs" {
+  name              = "${terraform.workspace}-${var.ecs_cluster_name}-logs"
+  retention_in_days = 0
+}
