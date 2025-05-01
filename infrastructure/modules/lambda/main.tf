feca3f3555bd995808662d87b022ee1c88102434
Matthew Moore <matthew.moore35@nhs.net>
2025-05-01 09:55:32 +0100

    [NDR-54] Add lifecycle blocks to Lambda resources to ignore changes during CI/CD deployments

--- infrastructure/modules/lambda/main.tf
+++ infrastructure/modules/lambda/main.tf
@@ -20,6 +20,14 @@ resource "aws_lambda_function" "lambda" {
     "arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension:53",
     "arn:aws:lambda:eu-west-2:282860088358:layer:AWS-AppConfig-Extension:120"
   ]
+
+  lifecycle {
+    ignore_changes = [
+      # These are required as Lambdas are deployed via the CI/CD pipelines
+      source_code_hash,
+      last_modified
+    ]
+  }
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
