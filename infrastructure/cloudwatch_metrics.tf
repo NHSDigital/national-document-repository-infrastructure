527c5a03a86797c1c59cfa00e4856d9c86a91501
robg-nhs <rob.gaskin@nhs.net>
2025-06-06 09:43:49 +0100

    [NDR-43] Log Group Metric Filters for PDS (#310)
    * [NDR-43] Terraform for PDS Event Count
    
    * [NDR-43] Declare a Local Array to group lambdas
    
    ---------
    
    Co-authored-by: Robert Gaskin <rob.gaskin1@nhs.net>

---
+++ infrastructure/cloudwatch_metrics.tf
@@ -0,0 +1,21 @@
+locals {
+  pds_tracking_lambdas = [
+    "SearchPatientDetailsLambda",
+    "BulkUploadLambda",
+    "MNSNotificationLambda"
+  ]
+}
+
+resource "aws_cloudwatch_log_metric_filter" "pds_tracker" {
+  for_each = toset(local.pds_tracking_lambdas)
+
+  name           = "PDSUsageMetricFilter-${each.key}"
+  pattern        = "%NDR-TR1%"
+  log_group_name = "/aws/lambda/${terraform.workspace}_${each.key}"
+
+  metric_transformation {
+    name      = "PDSEventCount"
+    namespace = "NDRInsights"
+    value     = "1"
+  }
+}
