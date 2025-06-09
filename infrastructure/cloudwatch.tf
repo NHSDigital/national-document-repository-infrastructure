f6cb4a9f85c0f7535eb1f11ff0a0a670dd93b7be
Sam Whyte <sam.whyte@answerdigital.com>
2025-06-09 18:44:17 +0100

    removing unused cloudwatch resource

--- infrastructure/cloudwatch.tf
+++
@@ -1,6 +0,0 @@
-module "cloudwatch" {
-  source                    = "./modules/cloudwatch"
-  cloudwatch_log_group_name = "/aws/api-gateway/access-logs"
-  environment               = var.environment
-  owner                     = var.owner
-}
