3158e6254d5aa7c5a8d41401c9703c9c664286bf
Sam Whyte <sam.whyte@answerdigital.com>
2025-06-23 10:56:31 +0100

    [NSR-121] table name to pascal

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -58,7 +58,7 @@ variable "statistical_reports_bucket_name" {
 variable "pdm_dynamodb_table_name" {
   type        = string
   description = "The name of the dynamodb table to be use for pdm metadata"
-  default     = "pdm_document_metadata"
+  default     = "pdm-document-metadata"
 }
 
 variable "docstore_dynamodb_table_name" {
