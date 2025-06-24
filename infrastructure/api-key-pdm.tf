2c1a33e7a06d915b932c2e391f92caf5313428c2
Sam Whyte <sam.whyte@answerdigital.com>
2025-06-24 15:18:16 +0100

    [NDR-121] underscores

--- infrastructure/api-key-pdm.tf
+++ infrastructure/api-key-pdm.tf
@@ -1,4 +1,4 @@
-resource "aws_api_gateway_usage_plan" "api-key-pdm" {
+resource "aws_api_gateway_usage_plan" "api_key_pdm" {
   name = "${terraform.workspace}_pdm-usage-plan"
   api_stages {
     api_id = aws_api_gateway_rest_api.ndr_doc_store_api.id
@@ -6,11 +6,11 @@ resource "aws_api_gateway_usage_plan" "api-key-pdm" {
   }
 }
 
-resource "aws_api_gateway_api_key" "api-key-pdm" {
+resource "aws_api_gateway_api_key" "api_key_pdm" {
   name = "${terraform.workspace}_pdm-api-key"
 }
 
-resource "aws_api_gateway_usage_plan_key" "api-key-pdm" {
+resource "aws_api_gateway_usage_plan_key" "api_key_pdm" {
   key_id        = aws_api_gateway_api_key.api-key-pdm.id
   key_type      = "API_KEY"
   usage_plan_id = aws_api_gateway_usage_plan.api-key-pdm.id
