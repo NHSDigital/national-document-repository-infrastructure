7aec0023bf59ef133efb12782a4c8c6238a8e329
NogaNHS <noga.sasson1@nhs.net>
2025-06-26 15:52:04 +0100

    [NDR-101] add filter prefix

--- infrastructure/lambda-document-upload-check.tf
+++ infrastructure/lambda-document-upload-check.tf
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_notification" "document_upload_check_lambda-lambda-trigg
   lambda_function {
     lambda_function_arn = module.document_upload_check_lambda.lambda_arn
     events              = ["s3:ObjectCreated:*"]
-
+    filter_prefix       = "user_upload"
   }
 }
 
