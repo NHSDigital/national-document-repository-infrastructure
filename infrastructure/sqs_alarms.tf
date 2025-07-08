1219b8d42abb901201fb7859bd32d836d8c053a9
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-07-08 16:32:06 +0100

    PRMT-466- updated todo

--- infrastructure/sqs_alarms.tf
+++ infrastructure/sqs_alarms.tf
@@ -13,13 +13,15 @@ locals {
     "stitching_dlq" = "${terraform.workspace}-deadletter-stitching-queue"
     "mns_dlq"       = "${terraform.workspace}-deadletter-mns-notification-queue"
   }
+  days_until_alarm = 6
 }
 locals {
   is_test_sandbox = contains([], terraform.workspace) # empty list disables sandbox detection, for testing only
 }
+# TODO: Delete is_test_sandbox, and change all call of is_test_sandbox to is_sandbox
 
 module "global_sqs_age_alarm_topic" {
-  count                  = local.is_test_sandbox ? 0 : 1
+  count                  = local.is_test_sandbox ? 0 : 1 # TODO:change is_test_sandbox to is_sandbox
   source                 = "./modules/sns"
   sns_encryption_key_id  = module.sns_encryption_key.id
   current_account_id     = data.aws_caller_identity.current.account_id
@@ -50,23 +52,23 @@ module "global_sqs_age_alarm_topic" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "sqs_oldest_message_alarm" {
-  for_each = local.is_test_sandbox ? {} : local.monitored_queues
+  for_each = local.is_test_sandbox ? {} : local.monitored_queues # TODO:change is_test_sandbox to is_sandbox
 
   alarm_name          = "${terraform.workspace}_${each.key}_oldest_message_alarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
   metric_name         = "ApproximateAgeOfOldestMessage"
   namespace           = "AWS/SQS"
-  period              = 60 # test (prod: 10800)
+  period              = 60 # TODO: change to 86400 (24h))
   statistic           = "Maximum"
-  threshold           = 60 # test (prod: 864000)
+  threshold           = 60 # TODO: change to local.days_until_alarm*24*60*60
   treat_missing_data  = "notBreaching"
 
   dimensions = {
     QueueName = each.value
   }
 
-  alarm_description = "Alarm when a message in queue '${each.value}' is older than 10 days."
+  alarm_description = "Alarm when a message in queue '${each.value}' is older than '${local.days_until_alarm}' days."
   alarm_actions     = [module.global_sqs_age_alarm_topic[0].arn]
 
   tags = {
@@ -77,7 +79,7 @@ resource "aws_cloudwatch_metric_alarm" "sqs_oldest_message_alarm" {
   }
 }
 resource "aws_sns_topic_subscription" "global_sqs_alarm_subscriptions" {
-  for_each  = local.is_test_sandbox ? toset([]) : toset(nonsensitive(split(",", data.aws_ssm_parameter.cloud_security_notification_email_list.value)))
+  for_each  = local.is_test_sandbox ? toset([]) : toset(nonsensitive(split(",", data.aws_ssm_parameter.cloud_security_notification_email_list.value))) # TODO:change is_test_sandbox to is_sandbox
   endpoint  = each.value
   protocol  = "email"
   topic_arn = module.global_sqs_age_alarm_topic[0].arn
