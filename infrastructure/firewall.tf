07d0a3e36e076e0bfacf7b8673161189334dc5ac
Sam Whyte <sam.whyte@answerdigital.com>
2025-07-16 11:08:55 +0100

    [NDR-144] remove sandbox block

--- infrastructure/firewall.tf
+++ infrastructure/firewall.tf
@@ -28,7 +28,7 @@ resource "aws_wafv2_web_acl_association" "web_acl_association" {
 resource "aws_wafv2_web_acl_association" "api_gateway" {
   resource_arn = aws_api_gateway_stage.ndr_api.arn
   web_acl_arn  = module.firewall_waf_v2_api[0].arn
-  count        = local.is_sandbox ? 0 : 1
+  # count        = local.is_sandbox ? 0 : 1
   depends_on = [
     aws_api_gateway_stage.ndr_api,
     module.firewall_waf_v2_api[0]
