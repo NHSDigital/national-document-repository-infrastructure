bbdf89ff39dc54c106e764866b753974e77eee67
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-07-18 09:58:14 +0100

    PRMT-462- updated call to iam

--- infrastructure/lambda-toggle-bulk-upload.tf
+++ infrastructure/lambda-toggle-bulk-upload.tf
@@ -5,8 +5,11 @@ module "toggle-bulk-upload-lambda" {
   lambda_timeout = 60
   memory_size    = 128
 
+  # iam_role_policy_documents = [
+  #   aws_iam_policy.lambda_toggle_bulk_upload_policy.arn
+  # ]
   iam_role_policy_documents = [
-    aws_iam_policy.lambda_toggle_bulk_upload_policy.arn
+  data.aws_iam_policy_document.lambda_toggle_bulk_upload_document.json
   ]
 
   lambda_environment_variables = {
