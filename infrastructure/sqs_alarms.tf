c77e995e722101bc310faaa22ac3555f0abea5f9
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-07-21 14:21:12 +0100

    PRMT-466- format code

--- infrastructure/sqs_alarms.tf
+++ infrastructure/sqs_alarms.tf
@@ -118,8 +118,8 @@ resource "aws_cloudwatch_metric_alarm" "sqs_oldest_message" {
     # alarm_group  =each.value
     # alarm_metric = "Has messages older than ${local.monitored_queue_day_list[count.index][2]} days"
     # alarm_metric = "${local.monitored_queue_day_list[count.index][2]}d"
-    alarm_metric           = "ApproximateAgeOfOldestMessage"
-    is_kpi = "true"
+    alarm_metric = "ApproximateAgeOfOldestMessage"
+    is_kpi       = "true"
   }
 }
 # resource "aws_sns_topic_subscription" "sqs_oldest_message_alarm" {
