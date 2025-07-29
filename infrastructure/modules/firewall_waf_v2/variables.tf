6b54f481c1d0229298b92aa30346d4f562771215
Sam Whyte <sam.whyte@answerdigital.com>
2025-07-29 11:53:38 +0100

    [NDR-182] rebase error

--- infrastructure/modules/firewall_waf_v2/variables.tf
+++ infrastructure/modules/firewall_waf_v2/variables.tf
@@ -14,12 +14,8 @@ variable "cloudfront_acl" {
 }
 
 variable "api" {
-  description = "True if using the firewall for an api - removes AWSBotControl."
-  type        = bool
-<<<<<<< HEAD
-=======
   description = "True if using the firewall for an api - removes AWSBotControl and SizeRestrictions_BODY"
->>>>>>> 0ec9ad0 ([NDR-182] exclude body rule)
+  type        = bool
   default     = false
 }
 
