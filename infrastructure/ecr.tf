1323e43b44f43c6c54d55c5184abeaa8d2e2eb43
Mohammad Iqbal <mohammad.iqbal27@nhs.net>
2025-07-30 12:20:52 +0100

    [PRMT-439] - Fix plan failure

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,7 +1,6 @@
 module "ndr-docker-ecr-ui" {
   source              = "./modules/ecr/"
   app_name            = "ndr-${terraform.workspace}-app"
-  current_account_id  = data.aws_caller_identity.current.account_id
   allow_force_destroy = local.is_force_destroy
 
   environment = var.environment
@@ -12,7 +11,6 @@ module "ndr-docker-ecr-data-collection" {
   count               = local.is_sandbox ? 0 : 1
   source              = "./modules/ecr/"
   app_name            = "${terraform.workspace}-data-collection"
-  current_account_id  = data.aws_caller_identity.current.account_id
   allow_force_destroy = local.is_force_destroy
 
   environment = var.environment
