35f5016f73e6b7cbdbfaf374c0d9db4e7f264953
jameslinnell <jimlinnell@gmail.com>
2025-08-01 11:42:54 +0100

    [NDR-161] (#359)
    * Add workspace as a default tag
    
    Add a resource group for clarity
    
    Remove tags that are populated by default tags
    
    Update Name tag on SG
    
    Add log groups for dev workspaces
    
    Add import step to workflow
    
    Add import step
    
    import lambda logs
    
    Remove commas from lambda list in import script
    
    Run Plan before import
    
    Add import to terraform
    
    Add proper id to import
    
    Add hardcoded import
    
    change ndrb to ndrd
    
    Remove import.tf
    
    Remove SearchFHIR import
    
    Add imports for ndra
    
    remove import step from workflow
    
    Add a script to list all resources
    
    import for ndra
    
    Remove terraform.workspace from imports
    
    ndra cloudwatch imports
    
    Fix WAFv2 list
    
    Remove imports for ndra
    
    Reverse check for non persistent environments
    
    Add Nhs Oauth import
    
    * Only import if sandbox
    
    * Remove imports
    
    * Create a log group cleanup script
    
    * Remove imports
    
    * Only delete lambda logs
    
    * Remove changes to Virus scanner as it will be deleted.
    
    * Set to delete all log groups in workspace
    
    * PR changes
    
    * Update variable description.

--- infrastructure/modules/lambda/variable.tf
+++ infrastructure/modules/lambda/variable.tf
@@ -94,3 +94,20 @@ variable "default_policies" {
   ]
 }
 
+variable "persistent_workspaces" {
+  type        = list(string)
+  description = "A list of workspaces that require persistent logs"
+  default     = ["ndr-dev", "ndr-test", "pre-prod", "prod"]
+}
+
+variable "default_lambda_layers" {
+  type = list(string)
+  default = [
+    "arn:aws:lambda:eu-west-2:282860088358:layer:AWS-AppConfig-Extension:120"
+  ]
+}
+
+variable "extra_lambda_layers" {
+  type    = list(string)
+  default = ["arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension:53"]
+}
