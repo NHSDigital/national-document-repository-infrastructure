312c9dab6c91d8f9e3f0bd518650a8d4a1904c87
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-08-05 08:49:52 +0100

    PRMT-466- updated

--- infrastructure/sqs_alarms.tf
+++ infrastructure/sqs_alarms.tf
@@ -1,18 +1,35 @@
 locals {
+  # monitored_queues = {
+  #   # main queues
+  #   "nrl_main"       = "${terraform.workspace}-nrl-queue.fifo"
+  #   "splunk_main"    = "${terraform.workspace}-splunk-queue"
+  #   "stitching_main" = "${terraform.workspace}-stitching-queue"
+  #   "lg_bulk_main"   = "${terraform.workspace}-lg-bulk-upload-metadata-queue.fifo"
+  #   "lg_inv_main"    = "${terraform.workspace}-lg-bulk-upload-invalid-queue"
+  #   "mns_main"       = "${terraform.workspace}-mns-notification-queue"
+  #
+  #   # dead-letter queues
+  #   "nrl_dlq"       = "${terraform.workspace}-deadletter-nrl-queue.fifo"
+  #   "stitching_dlq" = "${terraform.workspace}-deadletter-stitching-queue"
+  #   "mns_dlq"       = "${terraform.workspace}-deadletter-mns-notification-queue"
+  # }
+
+
   monitored_queues = {
     # main queues
-    "nrl_main"       = "${terraform.workspace}-nrl-queue.fifo"
-    "splunk_main"    = "${terraform.workspace}-splunk-queue"
-    "stitching_main" = "${terraform.workspace}-stitching-queue"
-    "lg_bulk_main"   = "${terraform.workspace}-lg-bulk-upload-metadata-queue.fifo"
-    "lg_inv_main"    = "${terraform.workspace}-lg-bulk-upload-invalid-queue"
-    "mns_main"       = "${terraform.workspace}-mns-notification-queue"
-
+    "nrl_main"       = module.sqs-nrl-queue.queue_name
+    "splunk_main" =    length(module.sqs-splunk-queue) > 0 ? module.sqs-splunk-queue[0].queue_name : null
+    "stitching_main" = module.sqs-stitching-queue.queue_name
+    "lg_bulk_main"   = module.sqs-lg-bulk-upload-metadata-queue.queue_name
+    "lg_inv_main"    = module.sqs-lg-bulk-upload-invalid-queue.queue_name
+    "mns_main"       = length(module.sqs-mns-notification-queue) > 0 ? module.sqs-mns-notification-queue[0].queue_name : null
     # dead-letter queues
-    "nrl_dlq"       = "${terraform.workspace}-deadletter-nrl-queue.fifo"
-    "stitching_dlq" = "${terraform.workspace}-deadletter-stitching-queue"
-    "mns_dlq"       = "${terraform.workspace}-deadletter-mns-notification-queue"
+    "nrl_dlq"        =module.sqs-nrl-queue.dlq_name
+    "stitching_dlq"  =module.sqs-stitching-queue.dlq_name
+    "mns_dlq"        =module.sqs-mns-notification-queue.dlq_name
   }
+
+
   days_until_alarm = [
     [6, "medium"],
     [10, "high"]
@@ -47,7 +64,7 @@ module "global_sqs_age_alarm_topic" {
   count                  = local.is_test_sandbox ? 0 : 1 # TODO:change is_test_sandbox to is_sandbox
   source                 = "./modules/sns"
   sns_encryption_key_id  = module.sns_encryption_key.id
-  current_account_id     = data.aws_caller_identity.current.account_id
+  # current_account_id     = data.aws_caller_identity.current.account_id
   topic_name             = "global-sqs-age-alarm-topic"
   topic_protocol         = "email"
   is_topic_endpoint_list = true
@@ -96,22 +113,19 @@ resource "aws_cloudwatch_metric_alarm" "sqs_oldest_message" {
   alarm_actions = [module.sqs_alarm_lambda_topic.arn]
   ok_actions    = [module.sqs_alarm_lambda_topic.arn]
 
-  tags = {
-    Name         = "${terraform.workspace}_${local.monitored_queue_day_list[count.index][0]}_oldest_message_alarm_${local.monitored_queue_day_list[count.index][2]}d"
-    Owner        = var.owner
-    Environment  = var.environment
-    Workspace    = terraform.workspace
-    severity     = local.monitored_queue_day_list[count.index][3]
-    alarm_group  = local.monitored_queue_day_list[count.index][1]
-    alarm_metric = "Has messages older than ${local.monitored_queue_day_list[count.index][2]} days"
-    is_kpi       = "true"
-  }
+ tags = {
+	    Name         = "${terraform.workspace}_${local.monitored_queue_day_list[count.index][0]}_oldest_message_alarm_${local.monitored_queue_day_list[count.index][2]}d"
+	    severity     = local.monitored_queue_day_list[count.index][3]
+	    alarm_group  = local.monitored_queue_day_list[count.index][1]
+	    alarm_metric = "ApproximateAgeOfOldestMessage"
+	    is_kpi       = "true"
+	  }
 }
 
 module "sqs_alarm_lambda_topic" {
-  source                = "../infrastructure/modules/sns"
+  source = "./modules/sns"
   sns_encryption_key_id = module.sns_encryption_key.id
-  current_account_id    = data.aws_caller_identity.current.account_id
+  # current_account_id    = data.aws_caller_identity.current.account_id
   topic_name            = "sqs-alarms-to-lambda-topic"
   topic_protocol        = "lambda"
   topic_endpoint        = module.im-alerting-lambda.lambda_arn
@@ -136,7 +150,7 @@ module "sqs_alarm_lambda_topic" {
   })
 }
 
-resource "aws_lambda_permission" "sqs_alerting_lambda_permission" {
+resource "aws_lambda_permission" "sqs_im_alerting" {
   statement_id  = "AllowExecutionFromSQSAlarmSNS"
   action        = "lambda:InvokeFunction"
   function_name = module.im-alerting-lambda.lambda_arn
