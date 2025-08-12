f1368eacfb7bc996e50ee54bc89de21d4abe1047
Megan <megan.bower4@nhs.net>
2025-08-12 14:17:54 +0100

    NDR-164 tf fmt

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -12,13 +12,13 @@ module "route53_fargate_ui" {
 }
 
 module "route53_mtls_api" {
-  source                        = "./modules/route53"
-  environment                   = var.environment
-  owner                         = var.owner
-  domain                        = var.domain
-  using_arf_hosted_zone         = true
-  dns_name                      = local.mtls_api_gateway_subdomain_name
-  api_gateway_subdomain_name    = local.mtls_api_gateway_subdomain_name
-  api_gateway_full_domain_name  = aws_api_gateway_domain_name.mtls_custom_api_domain.regional_domain_name
-  api_gateway_zone_id           = aws_api_gateway_domain_name.mtls_custom_api_domain.regional_zone_id
+  source                       = "./modules/route53"
+  environment                  = var.environment
+  owner                        = var.owner
+  domain                       = var.domain
+  using_arf_hosted_zone        = true
+  dns_name                     = local.mtls_api_gateway_subdomain_name
+  api_gateway_subdomain_name   = local.mtls_api_gateway_subdomain_name
+  api_gateway_full_domain_name = aws_api_gateway_domain_name.mtls_custom_api_domain.regional_domain_name
+  api_gateway_zone_id          = aws_api_gateway_domain_name.mtls_custom_api_domain.regional_zone_id
 }
