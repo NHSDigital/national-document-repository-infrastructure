973aa1fcb4c96bd7efa43bc03cb2acb5d866e018
samFallowfieldNHS <sam.fallowfield1@nhs.net>
2025-08-18 14:10:33 +0100

    added v2 lambdas and sqs queue update

---
+++ infrastructure/lambda-v2-bulk-upload-metadata.tf
@@ -0,0 +1,69 @@
+module "v2-bulk-upload-metadata-lambda" {
+  source         = "./modules/lambda"
+  name           = "V2BulkUploadMetadataLambda"
+  handler        = "handlers.bulk_upload_metadata_handler.lambda_handler"
+  lambda_timeout = 900
+  memory_size    = 1769
+  iam_role_policy_documents = [
+    module.ndr-bulk-staging-store.s3_read_policy_document,
+    module.ndr-bulk-staging-store.s3_write_policy_document,
+    module.sqs-lg-bulk-upload-metadata-queue.sqs_read_policy_document,
+    module.sqs-lg-bulk-upload-metadata-queue.sqs_write_policy_document,
+    module.ndr-app-config.app_config_policy
+  ]
+
+  rest_api_id       = null
+  api_execution_arn = null
+
+  lambda_environment_variables = {
+    APPCONFIG_APPLICATION     = module.ndr-app-config.app_config_application_id
+    APPCONFIG_ENVIRONMENT     = module.ndr-app-config.app_config_environment_id
+    APPCONFIG_CONFIGURATION   = module.ndr-app-config.app_config_configuration_profile_id
+    WORKSPACE                 = terraform.workspace
+    STAGING_STORE_BUCKET_NAME = "${terraform.workspace}-${var.staging_store_bucket_name}"
+    METADATA_SQS_QUEUE_URL    = module.sqs-lg-bulk-upload-metadata-queue.sqs_url
+  }
+  is_gateway_integration_needed = false
+  is_invoked_from_gateway       = false
+}
+
+module "v2-bulk-upload-metadata-alarm" {
+  source               = "./modules/lambda_alarms"
+  lambda_function_name = module.bulk-upload-metadata-lambda.function_name
+  lambda_timeout       = module.bulk-upload-metadata-lambda.timeout
+  lambda_name          = "bulk_upload_metadata_handler"
+  namespace            = "AWS/Lambda"
+  alarm_actions        = [module.v2-bulk-upload-metadata-alarm-topic.arn]
+  ok_actions           = [module.v2-bulk-upload-metadata-alarm-topic.arn]
+  depends_on           = [module.v2-bulk-upload-metadata-lambda, module.v2-bulk-upload-metadata-alarm-topic]
+}
+
+module "v2-bulk-upload-metadata-alarm-topic" {
+  source                = "./modules/sns"
+  sns_encryption_key_id = module.sns_encryption_key.id
+  topic_name            = "bulk-upload-metadata-topic"
+  topic_protocol        = "lambda"
+  topic_endpoint        = module.bulk-upload-metadata-lambda.lambda_arn
+  delivery_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudwatch.amazonaws.com"
+        },
+        "Action" : [
+          "SNS:Publish",
+        ],
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "arn:aws:cloudwatch:eu-west-2:${data.aws_caller_identity.current.account_id}:alarm:*"
+          }
+        }
+        "Resource" : "*"
+      }
+    ]
+  })
+
+  depends_on = [module.v2-bulk-upload-metadata-lambda, module.sns_encryption_key]
+}
--- infrastructure/queues.tf
+++ infrastructure/queues.tf
@@ -20,6 +20,19 @@ module "sqs-lg-bulk-upload-metadata-queue" {
   delay                = 60
 }
 
+module "v2-sqs-lg-bulk-upload-metadata-queue" {
+  source               = "./modules/sqs"
+  name                 = "v2-lg-bulk-upload-metadata-queue.fifo"
+  max_size_message     = 256 * 1024
+  message_retention    = 60 * 60 * 24 * 14
+  environment          = var.environment
+  owner                = var.owner
+  max_visibility       = 1020
+  enable_fifo          = true
+  enable_deduplication = true
+  delay                = 60
+}
+
 module "sqs-lg-bulk-upload-invalid-queue" {
   source            = "./modules/sqs"
   name              = "lg-bulk-upload-invalid-queue"
