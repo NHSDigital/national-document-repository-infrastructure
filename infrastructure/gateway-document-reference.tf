bfac07d135bc04c0b183a993721e20a4e4c425bc
jameslinnell <jimlinnell@gmail.com>
2025-09-09 12:19:10 +0100

    Revert "[NDR-205] Remove "count = 1" from any resources that have it. (#400)"
    This reverts commit 53ac70999e9be70954a68658d15e6eae920b353a.

--- infrastructure/gateway-document-reference.tf
+++ infrastructure/gateway-document-reference.tf
@@ -1,4 +1,5 @@
 module "fhir_document_reference_gateway" {
+  count               = 1
   source              = "./modules/gateway"
   api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
   parent_id           = aws_api_gateway_rest_api.ndr_doc_store_api.root_resource_id
@@ -8,4 +9,3 @@ module "fhir_document_reference_gateway" {
   gateway_path        = "FhirDocumentReference"
   require_credentials = true
 }
-
