12b4ebaa7357e8cf07fa4a6e72ffa7281cc7365c
jameslinnell <jimlinnell@gmail.com>
2025-09-09 15:45:49 +0100

    Add a ReadOnlyExtraAccess policy (#415)

--- infrastructure/policies.tf
+++ infrastructure/policies.tf
@@ -17,3 +17,26 @@ resource "aws_iam_policy" "ssm_access_policy" {
     ]
   })
 }
+
+resource "aws_iam_policy" "read_only_role_extra_permissions" {
+  name = "ReadOnlyExtraAccess"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "kms:Decrypt",
+        ],
+        Resource = [
+          "arn:aws:lambda:eu-west-2:${data.aws_caller_identity.current.account_id}:function:*",
+        ]
+      }
+    ]
+  })
+  tags = {
+    Name      = "ReadOnlyExtraAccess"
+    Workspace = "core"
+  }
+}
+
