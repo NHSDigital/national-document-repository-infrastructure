e4f9b0f060ea31c71acec9f6a342b2e5c6a6c4bb
Megan <megan.bower4@nhs.net>
2025-09-10 15:21:35 +0100

    NDR-213b Moved dots

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -23,7 +23,7 @@ variable "certificate_subdomain_name_prefix" {
 variable "certificate_subdomain_name_prefix_mtls" {
   description = "Prefix to add to subdomains on certification configurations."
   type        = string
-  default     = "mtls."
+  default     = "mtls"
 }
 
 # Bucket Variables
@@ -225,8 +225,8 @@ locals {
   api_gateway_subdomain_name   = contains(["prod"], terraform.workspace) ? "${var.certificate_subdomain_name_prefix}" : "${var.certificate_subdomain_name_prefix}${terraform.workspace}"
   api_gateway_full_domain_name = contains(["prod"], terraform.workspace) ? "${var.certificate_subdomain_name_prefix}${var.domain}" : "${var.certificate_subdomain_name_prefix}${terraform.workspace}.${var.domain}"
 
-  mtls_api_gateway_subdomain_name   = contains(["prod"], terraform.workspace) ? "${var.certificate_subdomain_name_prefix_mtls}" : "${var.certificate_subdomain_name_prefix_mtls}${terraform.workspace}"
-  mtls_api_gateway_full_domain_name = contains(["prod"], terraform.workspace) ? "${var.certificate_subdomain_name_prefix_mtls}${var.domain}" : "${var.certificate_subdomain_name_prefix_mtls}${terraform.workspace}.${var.domain}"
+  mtls_api_gateway_subdomain_name   = contains(["prod"], terraform.workspace) ? "${var.certificate_subdomain_name_prefix_mtls}." : "${var.certificate_subdomain_name_prefix_mtls}.${terraform.workspace}"
+  mtls_api_gateway_full_domain_name = contains(["prod"], terraform.workspace) ? "${var.certificate_subdomain_name_prefix_mtls}.${var.domain}" : "${var.certificate_subdomain_name_prefix_mtls}.${terraform.workspace}.${var.domain}"
 
   current_region     = data.aws_region.current.name
   current_account_id = data.aws_caller_identity.current.account_id
