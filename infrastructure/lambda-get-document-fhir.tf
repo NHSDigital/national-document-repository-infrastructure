082ef9b69c0a1df4afbf72c507b7e5380f490185
Megan <megan.bower4@nhs.net>
2025-10-13 08:43:45 +0100

    NDR-244 tf fmt

--- infrastructure/lambda-get-document-fhir.tf
+++ infrastructure/lambda-get-document-fhir.tf
@@ -22,10 +22,10 @@ resource "aws_api_gateway_method" "get_document_reference" {
 }
 
 resource "aws_api_gateway_method" "get_document_reference_mtls" {
-  rest_api_id      = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.id
-  resource_id      = aws_api_gateway_resource.get_document_reference_mtls.id
-  http_method      = "GET"
-  authorization    = "NONE"
+  rest_api_id   = aws_api_gateway_rest_api.ndr_doc_store_api_mtls.id
+  resource_id   = aws_api_gateway_resource.get_document_reference_mtls.id
+  http_method   = "GET"
+  authorization = "NONE"
   request_parameters = {
     "method.request.path.id" = true
   }
