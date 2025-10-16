8c1c92bc73eebc50c424792445622742888a7091
SWhyteAnswer <sam.whyte@answerdigital.com>
2025-10-16 17:00:12 +0100

    [PRMP-399] describe table policy

--- infrastructure/lambda-migration-dynamodb-segment.tf
+++ infrastructure/lambda-migration-dynamodb-segment.tf
@@ -6,7 +6,8 @@ module "migration-dynamodb-segment-lambda" {
   memory_size    = 1792
   iam_role_policy_documents = [
     module.migration-dynamodb-segment-store.s3_read_policy_document,
-    module.migration-dynamodb-segment-store.s3_write_policy_document
+    module.migration-dynamodb-segment-store.s3_write_policy_document,
+    data.aws_iam_policy_document.migration_dynamodb_access.json
   ]
   kms_deletion_window = var.kms_deletion_window
 
@@ -16,4 +17,16 @@ module "migration-dynamodb-segment-lambda" {
   }
   is_gateway_integration_needed = false
   is_invoked_from_gateway       = false
+}
+
+data "aws_iam_policy_document" "migration_dynamodb_access" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable"
+    ]
+    resources = [
+      "arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${terraform.workspace}_*"
+    ]
+  }
 }
