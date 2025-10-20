2e83d156ba767b3ec8813da15ce2864911d9124c
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-10-20 14:30:23 +0100

    PRMT-562 Created dynamodb migration lambda

--- infrastructure/lambda-dynamodb-migration.tf
+++ infrastructure/lambda-dynamodb-migration.tf
@@ -1,14 +1,13 @@
-module "migration-dynamodb-lambda" {
+module "dynamodb-migration-lambda" {
   source  = "./modules/lambda"
-  name    = "MigrationDynamoDB"
-  handler = "handlers.migration_dynamodb_handler.lambda_handler"
+  name    = "DynamoDBMigrationLambda"
+  handler = "handlers.dynamodb_migration_handler.lambda_handler"
 
   iam_role_policy_documents = [
     module.lloyd_george_reference_dynamodb_table.dynamodb_read_policy_document,
     module.lloyd_george_reference_dynamodb_table.dynamodb_write_policy_document,
     module.bulk_upload_report_dynamodb_table.dynamodb_read_policy_document,
-    module.ndr-bulk-staging-store.s3_read_policy_document,
-    module.ndr-lloyd-george-store.s3_read_policy_document,
+    module.bulk_upload_report_dynamodb_table.dynamodb_write_policy_document,
     aws_iam_policy.ssm_access_policy.policy,
     module.ndr-app-config.app_config_policy
   ]
@@ -35,5 +34,55 @@ module "migration-dynamodb-lambda" {
     module.bulk_upload_report_dynamodb_table,
     module.ndr-app-config,
     aws_iam_policy.ssm_access_policy,
+  ]
+}
+
+module "dynamodb-migration-alarm" {
+  source               = "./modules/lambda_alarms"
+  lambda_function_name = module.dynamodb-migration-lambda.function_name
+  lambda_timeout       = module.dynamodb-migration-lambda.timeout
+  lambda_name          = "dynamodb_migration_handler"
+  namespace            = "AWS/Lambda"
+
+  alarm_actions = [module.dynamodb-migration-alarm-topic.arn]
+  ok_actions    = [module.dynamodb-migration-alarm-topic.arn]
+
+  depends_on = [
+    module.dynamodb-migration-lambda,
+    module.dynamodb-migration-alarm-topic
+  ]
+}
+
+module "dynamodb-migration-alarm-topic" {
+  source                = "./modules/sns"
+  sns_encryption_key_id = module.sns_encryption_key.id
+  topic_name            = "dynamodb-migration-topic"
+  topic_protocol        = "lambda"
+  topic_endpoint        = module.dynamodb-migration-lambda.lambda_arn
+
+  delivery_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudwatch.amazonaws.com"
+        },
+        "Action" : [
+          "SNS:Publish"
+        ],
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "arn:aws:cloudwatch:eu-west-2:${data.aws_caller_identity.current.account_id}:alarm:*"
+          }
+        },
+        "Resource" : "*"
+      }
+    ]
+  })
+
+  depends_on = [
+    module.dynamodb-migration-lambda,
+    module.sns_encryption_key
   ]
 }
