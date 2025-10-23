60ba914293798db484dfa2de937603296b5e5c7a
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-10-23 12:02:13 +0100

    [PRMP-579] add count.index to outputs

--- infrastructure/modules/cloudfront/output.tf
+++ infrastructure/modules/cloudfront/output.tf
@@ -1,8 +1,8 @@
 output "cloudfront_url" {
-  value = aws_cloudfront_distribution.distribution.domain_name
+  value = aws_cloudfront_distribution.distribution[count.index].domain_name
 }
 
 output "cloudfront_arn" {
   description = "The ARN of the CloudFront Distribution"
-  value       = aws_cloudfront_distribution.distribution.arn
+  value       = aws_cloudfront_distribution.distribution[count.index].arn
 }
