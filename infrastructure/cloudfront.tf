1e1da9dea5d41603762e74660206a2bf1f82c044
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-10-24 09:17:18 +0100

    [PRMP-579] adjust path pattern on cloudfront distribution ordered cache behaviour

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -10,9 +10,9 @@ module "cloudfront_firewall_waf_v2" {
 
 module "cloudfront-distribution-lg" {
   has_secondary_bucket          = true
-  secondary_bucket_domain_name  = "${terraform.workspace}-${var.document_review_bucket_name}.s3.eu-west-2.amazonaws.com"
+  secondary_bucket_domain_name  = module.ndr-document-pending-review-store.bucket_domain_name
   secondary_bucket_id           = module.ndr-document-pending-review-store.bucket_id
-  secondary_bucket_path_pattern = "test/*"
+  secondary_bucket_path_pattern = "/test/*"
   source                        = "./modules/cloudfront"
   bucket_domain_name            = "${terraform.workspace}-${var.lloyd_george_bucket_name}.s3.eu-west-2.amazonaws.com"
   bucket_id                     = module.ndr-lloyd-george-store.bucket_id
