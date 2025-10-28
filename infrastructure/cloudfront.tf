31d935e81b371d613c4212609cbf34df9cc728e3
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-10-28 12:00:51 +0000

    [PRMP-579] address PR comments

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -9,14 +9,14 @@ module "cloudfront_firewall_waf_v2" {
 }
 
 module "cloudfront-distribution-lg" {
-  has_secondary_bucket          = true
-  secondary_bucket_domain_name  = module.ndr-document-pending-review-store.bucket_regional_domain_name
-  secondary_bucket_id           = module.ndr-document-pending-review-store.bucket_id
-  secondary_bucket_path_pattern = "/review/*"
   source                        = "./modules/cloudfront"
   bucket_domain_name            = module.ndr-lloyd-george-store.bucket_regional_domain_name
   bucket_id                     = module.ndr-lloyd-george-store.bucket_id
   qualifed_arn                  = module.edge-presign-lambda.qualified_arn
   depends_on                    = [module.edge-presign-lambda.qualified_arn, module.ndr-lloyd-george-store.bucket_id, module.ndr-lloyd-george-store.bucket_domain_name, module.ndr-document-pending-review-store.bucket_id, module.ndr-document-pending-review-store.bucket_domain_name]
   web_acl_id                    = try(module.cloudfront_firewall_waf_v2[0].arn, "")
+  has_secondary_bucket          = true
+  secondary_bucket_domain_name  = module.ndr-document-pending-review-store.bucket_regional_domain_name
+  secondary_bucket_id           = module.ndr-document-pending-review-store.bucket_id
+  secondary_bucket_path_pattern = "/review/*"
 }
