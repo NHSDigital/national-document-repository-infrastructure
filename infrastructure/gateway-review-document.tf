75381934e7fbd4ac7347894d2aa280d272e7589a
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-10-31 09:28:49 +0000

    [PRMP-586] adjust put doc lambda and gateway to handle path param

--- infrastructure/gateway-review-document.tf
+++ infrastructure/gateway-review-document.tf
@@ -2,7 +2,7 @@ module "review_document_gateway" {
   source              = "./modules/gateway"
   api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
   parent_id           = aws_api_gateway_rest_api.ndr_doc_store_api.root_resource_id
-  http_methods        = ["PUT", "GET"]
+  http_methods        = ["GET"]
   authorization       = "CUSTOM"
   gateway_path        = "DocumentReview"
   authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
@@ -14,7 +14,7 @@ module "review_document_id_gateway" {
   source              = "./modules/gateway"
   api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
   parent_id           = module.review_document_gateway.gateway_resource_id
-  http_methods        = ["GET"]
+  http_methods        = ["GET", "PUT"]
   authorization       = "CUSTOM"
   gateway_path        = "{id}"
   authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
