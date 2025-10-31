75270071b339c724be56713d79b6b8967f6248f4
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-10-31 14:33:46 +0000

    [PRMT-374] deploy jsons as default for the aliases

--- infrastructure/buckets.tf
+++ infrastructure/buckets.tf
@@ -419,18 +419,18 @@ resource "aws_s3_object" "metadata_aliases_general" {
   bucket = module.ndr-configs-store.bucket_id
   key    = "metadata_aliases/general/general.json"
 
-  source        = "${path.module}/modules/aliases_config/general/general.json"
-  etag          = filemd5("${path.module}/modules/aliases_config/general/general.json")
-  content_type  = "application/json"
+  source       = "${path.module}/modules/aliases_config/general/general.json"
+  etag         = filemd5("${path.module}/modules/aliases_config/general/general.json")
+  content_type = "application/json"
 }
 
 resource "aws_s3_object" "metadata_aliases_usb" {
   bucket = module.ndr-configs-store.bucket_id
   key    = "metadata_aliases/usb/usb.json"
 
-  source        = "${path.module}/modules/aliases_config/usb/usb.json"
-  etag          = filemd5("${path.module}/modules/aliases_config/usb/usb.json")
-  content_type  = "application/json"
+  source       = "${path.module}/modules/aliases_config/usb/usb.json"
+  etag         = filemd5("${path.module}/modules/aliases_config/usb/usb.json")
+  content_type = "application/json"
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "configs-store-lifecycle-rules" {
