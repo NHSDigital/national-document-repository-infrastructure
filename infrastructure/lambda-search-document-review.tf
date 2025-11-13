8b76092e80a4a9a772c041743ddbdd53d97ce0e6
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2025-11-13 14:50:37 +0000

    [PRMP-600] Update DynamoDB table references to use index notation
    Signed-off-by: NogaNHS <127490765+NogaNHS@users.noreply.github.com>

--- infrastructure/lambda-search-document-review.tf
+++ infrastructure/lambda-search-document-review.tf
@@ -4,7 +4,7 @@ module "search_document_review_lambda" {
   handler = "handlers.search_document_review_handler.lambda_handler"
   iam_role_policy_documents = [
     module.ndr-app-config.app_config_policy,
-    module.document_review_dynamodb_table.dynamodb_read_policy_document
+    module.document_review_dynamodb_table[0].dynamodb_read_policy_document
   ]
 
   rest_api_id                   = aws_api_gateway_rest_api.ndr_doc_store_api.id
@@ -18,7 +18,7 @@ module "search_document_review_lambda" {
     APPCONFIG_APPLICATION         = module.ndr-app-config.app_config_application_id
     APPCONFIG_ENVIRONMENT         = module.ndr-app-config.app_config_environment_id
     APPCONFIG_CONFIGURATION       = module.ndr-app-config.app_config_configuration_profile_id
-    DOCUMENT_REVIEW_DYNAMODB_NAME = module.document_review_dynamodb_table.table_name
+    DOCUMENT_REVIEW_DYNAMODB_NAME = module.document_review_dynamodb_table[0].table_name
     WORKSPACE                     = terraform.workspace
   }
   depends_on = [
