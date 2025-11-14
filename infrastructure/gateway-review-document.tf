22c9bcfe2bf067711c2bf378dc6b936988ff410f
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-11-14 14:29:12 +0000

    [PRMP-817] add new API endpoint

--- infrastructure/gateway-review-document.tf
+++ infrastructure/gateway-review-document.tf
@@ -25,3 +25,19 @@ module "review_document_id_gateway" {
     "method.request.path.id" = true
   }
 }
+
+module "review_document_status_id" {
+  source              = "./modules/gateway"
+  api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  parent_id           = module.review_document_gateway.gateway_resource_id
+  http_methods        = ["GET"]
+  authorization       = "CUSTOM"
+  gateway_path        = "{id}"
+  authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
+  require_credentials = true
+  origin              = contains(["prod"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
+
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
