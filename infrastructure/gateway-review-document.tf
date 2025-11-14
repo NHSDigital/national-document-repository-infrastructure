bdb41ca511e71ac11ee3e5989e81f49c69395ad6
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-11-14 17:43:25 +0000

    [PRMP-817] add parent gateway

--- infrastructure/gateway-review-document.tf
+++ infrastructure/gateway-review-document.tf
@@ -26,10 +26,22 @@ module "review_document_id_gateway" {
   }
 }
 
-module "review_document_status_id_gateway" {
+module "review_document_status_gateway" {
   source              = "./modules/gateway"
   api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
   parent_id           = module.review_document_gateway.gateway_resource_id
+  gateway_path        = "Status"
+  http_methods        = []
+  require_credentials = true
+  authorization       = "CUSTOM"
+  authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
+  origin              = contains(["prod"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
+}
+
+module "review_document_status_id_gateway" {
+  source              = "./modules/gateway"
+  api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  parent_id           = module.review_document_status_gateway.gateway_resource_id
   http_methods        = ["GET"]
   authorization       = "CUSTOM"
   gateway_path        = "{id}"
