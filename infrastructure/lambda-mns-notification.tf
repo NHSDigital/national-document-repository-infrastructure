427dc8d720c76e131aafdd7ee57b3a672ba7a8c5
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2025-11-17 11:20:15 +0000

    remove not
    Signed-off-by: NogaNHS <127490765+NogaNHS@users.noreply.github.com>

--- infrastructure/lambda-mns-notification.tf
+++ infrastructure/lambda-mns-notification.tf
@@ -9,7 +9,7 @@ module "mns-notification-lambda" {
     module.lloyd_george_reference_dynamodb_table.dynamodb_write_policy_document,
     module.lloyd_george_reference_dynamodb_table.dynamodb_read_policy_document,
     local.is_production ? null : module.document_review_dynamodb_table[0].dynamodb_write_policy_document,
-    !local.is_production ? null : module.document_review_dynamodb_table[0].dynamodb_read_policy_document,
+    local.is_production ? null : module.document_review_dynamodb_table[0].dynamodb_read_policy_document,
     aws_iam_policy.ssm_access_policy.policy,
     module.ndr-app-config.app_config_policy,
     aws_iam_policy.kms_mns_lambda_access[0].policy,
