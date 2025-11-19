fcb0b027b879237d3ef165f39237bf58ebd9e96e
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2025-11-19 15:07:05 +0000

    Refactor API Gateway resource for document versioning and update request parameters
    Signed-off-by: NogaNHS <127490765+NogaNHS@users.noreply.github.com>

--- infrastructure/gateway-review-document.tf
+++ infrastructure/gateway-review-document.tf
@@ -10,18 +10,25 @@ module "review_document_gateway" {
   origin              = contains(["prod"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
 }
 
-module "review_document_id_gateway" {
+resource "aws_api_gateway_resource" "review_document_id" {
+  rest_api_id = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  parent_id   = module.review_document_gateway.gateway_resource_id
+  path_part   = "{id}"
+}
+
+module "review_document_version_gateway" {
   source              = "./modules/gateway"
   api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
-  parent_id           = module.review_document_gateway.gateway_resource_id
+  parent_id           = aws_api_gateway_resource.review_document_id.id
   http_methods        = ["GET", "PATCH"]
   authorization       = "CUSTOM"
-  gateway_path        = "{id}/{version}"
+  gateway_path        = "{version}"
   authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
   require_credentials = true
   origin              = contains(["prod"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
 
   request_parameters = {
-    "method.request.path.id" = true
+    "method.request.path.id"      = true
+    "method.request.path.version" = true
   }
 }
