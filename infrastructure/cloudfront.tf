d9a2565136624d2ba2115c35f362b6461f09eda4
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-11-20 14:29:16 +0000

    [PRMP-579] revert condition flip

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -15,7 +15,7 @@ module "cloudfront-distribution-lg" {
   qualifed_arn                  = module.edge-presign-lambda.qualified_arn
   depends_on                    = [module.edge-presign-lambda.qualified_arn, module.ndr-lloyd-george-store.bucket_id, module.ndr-lloyd-george-store.bucket_domain_name, module.ndr-document-pending-review-store.bucket_id, module.ndr-document-pending-review-store.bucket_domain_name]
   web_acl_id                    = try(module.cloudfront_firewall_waf_v2[0].arn, "")
-  has_secondary_bucket          = local.is_production ? true : false
+  has_secondary_bucket          = local.is_production ? false : true
   secondary_bucket_domain_name  = module.ndr-document-pending-review-store.bucket_regional_domain_name
   secondary_bucket_id           = module.ndr-document-pending-review-store.bucket_id
   secondary_bucket_path_pattern = "/review/*"
