f79eb4715da7f6c875332a01da482c317b2f75de
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-11-25 14:34:43 +0000

    [PRMT-862] removed the need for a SSM variable for transfer family

--- infrastructure/lambda-transfer-kill-switch.tf
+++ infrastructure/lambda-transfer-kill-switch.tf
@@ -1,13 +1,11 @@
 module "transfer_kill_switch_lambda" {
-  # TODO for testing purposes have it has 1 and non testing purposes as local.is_production ? 1 : 0
-  count   = 1 #local.is_production ? 1 : 0
+  count   = 1
   source  = "./modules/lambda"
   name    = "TransferKillSwitch"
   handler = "handlers.transfer_kill_switch_handler.lambda_handler"
 
   iam_role_policy_documents = [
     aws_iam_policy.transfer_kill_switch_policy.policy,
-    aws_iam_policy.ssm_access_policy.policy,
     data.aws_iam_policy.aws_lambda_vpc_access_execution_role.policy,
   ]
 
@@ -21,14 +19,13 @@ module "transfer_kill_switch_lambda" {
   lambda_environment_variables = {
     WORKSPACE                    = terraform.workspace
     STAGING_STORE_BUCKET_NAME    = "${terraform.workspace}-${var.staging_store_bucket_name}"
-    TRANSFER_SERVER_ID_SSM_PARAM = "/prs/${var.environment}/transfer-server-id"
   }
 
   is_gateway_integration_needed = false
   is_invoked_from_gateway       = false
 
   lambda_timeout                 = 60
-  memory_size                    = 138
+  memory_size                    = 125
   reserved_concurrent_executions = -1
 
   vpc_subnet_ids         = length(data.aws_security_groups.virus_scanner_api.ids) == 1 ? module.ndr-vpc-ui.private_subnets : []
