aa7b90569efc49d10e2c047aa881fc601f0883ff
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-11-25 12:28:34 +0000

    [PRMP-586] Create GET and PATCH/DocumentReview API endpoints and Lambdas (#482)
    Co-authored-by: NogaNHS <127490765+NogaNHS@users.noreply.github.com>

---
+++ infrastructure/lambda-patch-document-review.tf
@@ -0,0 +1,72 @@
+module "patch_document_review_lambda" {
+  source  = "./modules/lambda"
+  name    = "PatchDocumentReview"
+  handler = "handlers.patch_document_review_handler.lambda_handler"
+  iam_role_policy_documents = [
+    module.ndr-app-config.app_config_policy,
+    local.is_production ? "" : module.document_review_dynamodb_table[0].dynamodb_write_policy_document,
+    local.is_production ? "" : module.document_review_dynamodb_table[0].dynamodb_read_policy_document,
+    aws_iam_policy.ssm_access_policy.policy,
+    module.ndr-document-pending-review-store.s3_write_policy_document
+  ]
+
+  rest_api_id                   = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  api_execution_arn             = aws_api_gateway_rest_api.ndr_doc_store_api.execution_arn
+  http_methods                  = ["PATCH"]
+  resource_id                   = module.review_document_version_gateway.gateway_resource_id
+  kms_deletion_window           = var.kms_deletion_window
+  is_gateway_integration_needed = true
+  is_invoked_from_gateway       = true
+  lambda_environment_variables = {
+    APPCONFIG_APPLICATION         = module.ndr-app-config.app_config_application_id
+    APPCONFIG_ENVIRONMENT         = module.ndr-app-config.app_config_environment_id
+    APPCONFIG_CONFIGURATION       = module.ndr-app-config.app_config_configuration_profile_id
+    DOCUMENT_REVIEW_DYNAMODB_NAME = local.is_production ? "" : module.document_review_dynamodb_table[0].table_name
+    WORKSPACE                     = terraform.workspace
+  }
+  depends_on = [
+    aws_api_gateway_rest_api.ndr_doc_store_api,
+    module.review_document_version_gateway
+  ]
+}
+
+
+module "patch_document_review_lambda_alarm" {
+  source               = "./modules/lambda_alarms"
+  lambda_function_name = module.patch_document_review_lambda.function_name
+  lambda_timeout       = module.patch_document_review_lambda.timeout
+  lambda_name          = "patch_document_review_handler"
+  namespace            = "AWS/Lambda"
+  alarm_actions        = [module.patch_document_review_lambda_alarm_topic.arn]
+  ok_actions           = [module.patch_document_review_lambda_alarm_topic.arn]
+}
+
+
+module "patch_document_review_lambda_alarm_topic" {
+  source                = "./modules/sns"
+  sns_encryption_key_id = module.sns_encryption_key.id
+  topic_name            = "patch-document-review-lambda-alarm-topic"
+  topic_protocol        = "lambda"
+  topic_endpoint        = module.patch_document_review_lambda.lambda_arn
+  delivery_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudwatch.amazonaws.com"
+        },
+        "Action" : [
+          "SNS:Publish",
+        ],
+        "Condition" : {
+          "ArnLike" : {
+            "aws:SourceArn" : "arn:aws:cloudwatch:eu-west-2:${data.aws_caller_identity.current.account_id}:alarm:*"
+          }
+        }
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
