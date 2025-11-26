0441e6101d25d3e561f007567ce839c61791f0c0
NogaNHS <127490765+NogaNHS@users.noreply.github.com>
2025-11-26 15:07:27 +0000

    [PRMP-809] Add S3 event trigger for document upload check lambda with review prefix
    Signed-off-by: NogaNHS <127490765+NogaNHS@users.noreply.github.com>

--- infrastructure/lambda-document-upload-check.tf
+++ infrastructure/lambda-document-upload-check.tf
@@ -66,6 +66,11 @@ resource "aws_s3_bucket_notification" "document_upload_check_lambda_trigger" {
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "fhir_upload"
   }
+  lambda_function {
+    lambda_function_arn = module.document_upload_check_lambda.lambda_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "review"
+  }
 }
 
 resource "aws_lambda_permission" "document_upload_check_lambda" {
