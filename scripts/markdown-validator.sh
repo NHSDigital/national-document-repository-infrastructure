f2bc10f49ff2c18221b86584b7335249ac490982
chrisbloe <kris.bloe2@nhs.net>
2025-11-27 12:32:32 +0000

    Comment out the check until the git hook is ready

--- scripts/markdown-validator.sh
+++ scripts/markdown-validator.sh
@@ -11,8 +11,8 @@
 # Please, make sure to enable Markdown linting in your IDE. For the Visual Studio Code editor it is
 # `davidanson.vscode-markdownlint` that is already specified in the `.vscode/extensions.json` file.
 
-#files=$((git diff --diff-filter=ACMRT --name-only origin/${BRANCH_NAME:-main}.. "*.md"; git diff --name-only "*.md") | sort | uniq)
-files=$((find . -name "*.md" -printf '%P\n') | sort | uniq)
+files=$((git diff --diff-filter=ACMRT --name-only origin/${BRANCH_NAME:-main}.. "*.md"; git diff --name-only "*.md") | sort | uniq)
+#files=$((find . -name "*.md" -printf '%P\n') | sort | uniq)
 
 if [ -n "$files" ]; then
   image=ghcr.io/igorshubovych/markdownlint-cli@sha256:b771097d3d9ec6b368fefcea5f0c05c8710293d9251fca17e31c8e10987b50ef # v0.46.0
