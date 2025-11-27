78387b6451745fcec5e6914e7e82b909a153bd19
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-11-27 09:23:51 +0000

    [PRMT-862] fixed comments

--- infrastructure/lambda-transfer-kill-switch.tf
+++ infrastructure/lambda-transfer-kill-switch.tf
@@ -1,5 +1,4 @@
 module "transfer_kill_switch_lambda" {
-  count   = 1
   source  = "./modules/lambda"
   name    = "TransferKillSwitch"
   handler = "handlers.transfer_kill_switch_handler.lambda_handler"
@@ -11,11 +10,6 @@ module "transfer_kill_switch_lambda" {
 
   kms_deletion_window = var.kms_deletion_window
 
-  rest_api_id       = null
-  resource_id       = null
-  http_methods      = []
-  api_execution_arn = null
-
   lambda_environment_variables = {
     WORKSPACE                 = terraform.workspace
     STAGING_STORE_BUCKET_NAME = "${terraform.workspace}-${var.staging_store_bucket_name}"
@@ -24,9 +18,6 @@ module "transfer_kill_switch_lambda" {
   is_gateway_integration_needed = false
   is_invoked_from_gateway       = false
 
-  lambda_timeout                 = 60
-  memory_size                    = 128
-  reserved_concurrent_executions = -1
 
   vpc_subnet_ids         = length(data.aws_security_groups.virus_scanner_api.ids) == 1 ? module.ndr-vpc-ui.private_subnets : []
   vpc_security_group_ids = length(data.aws_security_groups.virus_scanner_api.ids) == 1 ? [data.aws_security_groups.virus_scanner_api.ids[0]] : []
