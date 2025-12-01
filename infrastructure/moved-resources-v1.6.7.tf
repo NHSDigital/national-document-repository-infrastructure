1c7108dd900befbe9e40e9b4dcf3d15c8588b8ad
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-12-01 11:19:19 +0000

    [PRMP-892] add moved block for distribution

--- infrastructure/moved-resources-v1.6.7.tf
+++ infrastructure/moved-resources-v1.6.7.tf
@@ -1,8 +1,8 @@
 #PRMP-579
-# moved {
-#   from = aws_cloudfront_distribution.distribution[0]
-#   to   = aws_cloudfront_distribution.distribution
-# }
+moved {
+  from = module.cloudfront-distribution-lg.aws_cloudfront_distribution.distribution[0]
+  to   = aws_cloudfront_distribution.distribution
+}
 
 moved {
   from = module.cloudfront-distribution-lg.aws_cloudfront_origin_access_control.cloudfront_s3_oac
