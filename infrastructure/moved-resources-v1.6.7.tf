30de7db037e79256f147e9e8151dcadea950e17a
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-12-01 11:14:57 +0000

    [PRMP-892] include resource type in moved blocks

--- infrastructure/moved-resources-v1.6.7.tf
+++ infrastructure/moved-resources-v1.6.7.tf
@@ -1,20 +1,20 @@
 #PRMP-579
-moved {
-  from = aws_cloudfront_distribution.distribution[0]
-  to   = aws_cloudfront_distribution.distribution
-}
+# moved {
+#   from = aws_cloudfront_distribution.distribution[0]
+#   to   = aws_cloudfront_distribution.distribution
+# }
 
 moved {
-  from = module.cloudfront-distribution-lg.cloudfront_s3_oac
+  from = module.cloudfront-distribution-lg.aws_cloudfront_origin_access_control.cloudfront_s3_oac
   to   = aws_cloudfront_origin_access_control.cloudfront_s3_oac
 }
 
 moved {
-  from = module.cloudfront-distribution-lg.nocache
+  from = module.cloudfront-distribution-lg.aws_cloudfront_cache_policy.nocache
   to   = aws_cloudfront_cache_policy.nocache
 }
 
 moved {
-  from = module.cloudfront-distribution-lg.viewer_policy
+  from = module.cloudfront-distribution-lg.aws_cloudfront_origin_request_policy.viewer_policy
   to   = aws_cloudfront_origin_request_policy.viewer_policy
 }
