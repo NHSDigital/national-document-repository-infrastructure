a9f1f5904d30dc11f5688ae119257defbfe7e87d
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-12-01 09:43:58 +0000

    [PRMP-892] try moved blocks

--- infrastructure/moved-resources-v1.6.7.tf
+++ infrastructure/moved-resources-v1.6.7.tf
@@ -3,3 +3,18 @@
 #   from = aws_cloudfront_distribution.distribution
 #   to   = aws_cloudfront_distribution.distribution[0]
 # }
+
+moved {
+  from = aws_cloudfront_origin_access_control.cloudfront_s3_oac
+  to   = aws_cloudfront_origin_access_control.cloudfront_s3_oac
+}
+
+moved {
+  from = aws_cloudfront_cache_policy.nocache
+  to   = aws_cloudfront_cache_policy.nocache
+}
+
+moved {
+  from = aws_cloudfront_origin_request_policy.viewer_policy
+  to   = aws_cloudfront_origin_request_policy.viewer_policy
+}
