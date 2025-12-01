c9418d9fee0117bf1706473d7b360f7bd41b649d
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-12-01 12:09:21 +0000

    [PRMP-892] reverse distribution moved block

--- infrastructure/moved-resources-v1.6.8.tf
+++ infrastructure/moved-resources-v1.6.8.tf
@@ -1,13 +1,13 @@
 #PRMP-892
-# moved {
-#   from = module.cloudfront-distribution-lg.aws_cloudfront_distribution.distribution[0]
-#   to   = aws_cloudfront_distribution.distribution
-# }
-
 moved {
-  from = module.cloudfront-distribution-lg.aws_cloudfront_distribution.distribution_with_secondary_bucket[0]
+  from = module.cloudfront-distribution-lg.aws_cloudfront_distribution.distribution[0]
   to   = aws_cloudfront_distribution.distribution
 }
+
+# moved {
+#   from = module.cloudfront-distribution-lg.aws_cloudfront_distribution.distribution_with_secondary_bucket[0]
+#   to   = aws_cloudfront_distribution.distribution
+# }
 
 
 moved {
