010131592db278cf0fe47286ed029b2d968e0b1b
Tim Knight <tim.knight@airelogic.com>
2025-12-12 12:07:20 +0000

    [ndr-297] updated to remote state. plan only.

--- pre_core/variables.tf
+++ pre_core/variables.tf
@@ -9,7 +9,24 @@ variable "region" {
   description = "The region to be used for bootstrapping"
 }
 
-variable "aws_account_id" {
+# variable "aws_account_id" {
+#   type        = string
+#   description = "The AWS Account ID (numeric)"
+# }
+
+variable "owner" {
+  description = "Identifies the team or person responsible for the resource (used for tagging)."
   type        = string
-  description = "The AWS Account ID (numeric)"
+  default     = "nhse/ndr-team"
+}
+
+locals {
+  is_sandbox    = !contains(["ndr-dev", "ndr-test", "pre-prod", "prod"], terraform.workspace)
+  is_production = contains(["pre-prod", "prod"], terraform.workspace)
+
+  is_sandbox_or_dev = !contains(["ndr-test", "pre-prod", "prod"], terraform.workspace)
+  is_development    = terraform.workspace == "ndr-dev"
+  is_testing        = terraform.workspace == "ndr-test"
+  is_pre_production = terraform.workspace == "pre-prod"
+  is_prod           = terraform.workspace == "prod"
 }
