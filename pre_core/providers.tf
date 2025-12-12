e7b9f1d65c7fa7269478772663a0f1229236b35a
Tim Knight <tim.knight@airelogic.com>
2025-12-12 13:26:12 +0000

    [ndr-297] refactoring of script

--- pre_core/providers.tf
+++ pre_core/providers.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = var.region
-  allowed_account_ids = [
-    data.aws_caller_identity.current.account_id,
-  ]
+  # allowed_account_ids = [
+  #   data.aws_caller_identity.current.account_id,
+  # ]
   default_tags {
     tags = {
       Owner       = var.owner
