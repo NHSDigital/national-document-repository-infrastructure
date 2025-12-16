83ff9c9b075962131b9ea6b20d23866b5e2dbbb7
SWhyteAnswer <sam.whyte@answerdigital.com>
2025-12-16 15:59:01 +0000

    [PRMP-1048] changing cron for testing

--- infrastructure/concurrency_controls.tf
+++ infrastructure/concurrency_controls.tf
@@ -1,8 +1,10 @@
 # Concurrency control schedules 
 # Office hour start 
+# Original office hours schedule (9 AM daily)
+# schedule_expression = "cron(0 9 * * ? *)"
 resource "aws_cloudwatch_event_rule" "bulk_upload_concurrency_office_hours_start" {
   name                = "bulk-upload-office-hours-start"
-  schedule_expression = "cron(0 9 * * ? *)"
+  schedule_expression = "cron(0/2 * * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "bulk_upload_concurrency_office_hours_start" {
@@ -17,9 +19,11 @@ resource "aws_cloudwatch_event_target" "bulk_upload_concurrency_office_hours_sta
 }
 
 # Office hours stop 
+# Original office hours schedule (5 PM daily)
+# schedule_expression = "cron(0 17 * * ? *)"
 resource "aws_cloudwatch_event_rule" "bulk_upload_concurrency_office_hours_stop" {
   name                = "bulk-upload-office-hours-stop"
-  schedule_expression = "cron(0 17 * * ? *)"
+  schedule_expression = "cron(1/2 * * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "bulk_upload_concurrency_office_hours_stop" {
