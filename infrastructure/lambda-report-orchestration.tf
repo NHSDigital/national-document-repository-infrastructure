47b014c3ca8a325d06bc1a97b3b734a0bb29064c
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-12-17 13:44:13 +0000

    [PRMP-1054] create report orchestration lambda

---
+++ infrastructure/lambda-report-orchestration.tf
@@ -0,0 +1,27 @@
+module "report-orchestration-lambda" {
+  source         = "./modules/lambda"
+  name           = "reportOrchestration"
+  handler        = "handlers.report_orchestration_handler.lambda_handler"
+  lambda_timeout = 900
+  memory_size    = 1769
+
+  iam_role_policy_documents = [
+    module.bulk_upload_report_dynamodb_table.dynamodb_read_policy_document,
+    module.bulk_upload_report_dynamodb_table.dynamodb_write_policy_document,
+    data.aws_iam_policy.aws_lambda_vpc_access_execution_role.policy,
+  ]
+
+  lambda_environment_variables = {
+    APPCONFIG_APPLICATION      = module.ndr-app-config.app_config_application_id
+    APPCONFIG_ENVIRONMENT      = module.ndr-app-config.app_config_environment_id
+    APPCONFIG_CONFIGURATION    = module.ndr-app-config.app_config_configuration_profile_id
+    WORKSPACE                  = terraform.workspace
+
+    BULK_UPLOAD_REPORT_TABLE_NAME = "${terraform.workspace}_BulkUploadReport"
+  }
+
+  rest_api_id                   = null
+  api_execution_arn             = null
+  is_gateway_integration_needed = false
+  is_invoked_from_gateway       = false
+}
