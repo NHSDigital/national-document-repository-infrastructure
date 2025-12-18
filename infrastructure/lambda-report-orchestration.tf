3f19e7ca7f59d1ae4dc03f49b472152f7469abe0
PedroSoaresNHS <pedro.soares1@nhs.net>
2025-12-18 14:18:41 +0000

    [PRMP-1054] trying to fix terraform

--- infrastructure/lambda-report-orchestration.tf
+++ infrastructure/lambda-report-orchestration.tf
@@ -1,7 +1,7 @@
 module "report-orchestration-lambda" {
-  source         = "./modules/lambda"
-  name           = "reportOrchestration"
-  handler        = "handlers.report_orchestration_handler.lambda_handler"
+  source  = "./modules/lambda"
+  name    = "reportOrchestration"
+  handler = "handlers.report_orchestration_handler.lambda_handler"
   lambda_timeout = 900
   memory_size    = 1769
 
@@ -16,7 +16,6 @@ module "report-orchestration-lambda" {
     APPCONFIG_ENVIRONMENT   = module.ndr-app-config.app_config_environment_id
     APPCONFIG_CONFIGURATION = module.ndr-app-config.app_config_configuration_profile_id
     WORKSPACE               = terraform.workspace
-
     BULK_UPLOAD_REPORT_TABLE_NAME = "${terraform.workspace}_BulkUploadReport"
   }
 
@@ -24,4 +23,12 @@ module "report-orchestration-lambda" {
   api_execution_arn             = null
   is_gateway_integration_needed = false
   is_invoked_from_gateway       = false
+
+  depends_on = [
+    module.bulk_upload_report_dynamodb_table
+  ]
+}
+
+data "aws_iam_policy" "aws_lambda_vpc_access_execution_role" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
