71268efe3ab14d433f54c82570520381ee0ddd11
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2025-12-18 10:09:01 +0000

    [PRMP-892] Add Staging S3 Bucket as an Origin to the CloudFront Distribution (#527)
    Signed-off-by: NogaNHS <127490765+NogaNHS@users.noreply.github.com>
    Co-authored-by: NogaNHS <127490765+NogaNHS@users.noreply.github.com>

---
+++ infrastructure/moved-resources-v1.6.9.tf
@@ -0,0 +1,25 @@
+#PRMP-892
+# moved {
+#   from = module.cloudfront-distribution-lg.aws_cloudfront_distribution.distribution[0]
+#   to   = aws_cloudfront_distribution.s3_presign_mask
+# }
+
+moved {
+  from = module.cloudfront-distribution-lg.aws_cloudfront_distribution.distribution_with_secondary_bucket[0]
+  to   = aws_cloudfront_distribution.s3_presign_mask
+}
+
+moved {
+  from = module.cloudfront-distribution-lg.aws_cloudfront_origin_access_control.cloudfront_s3_oac
+  to   = aws_cloudfront_origin_access_control.s3
+}
+
+moved {
+  from = module.cloudfront-distribution-lg.aws_cloudfront_cache_policy.nocache
+  to   = aws_cloudfront_cache_policy.nocache
+}
+
+moved {
+  from = module.cloudfront-distribution-lg.aws_cloudfront_origin_request_policy.viewer_policy
+  to   = aws_cloudfront_origin_request_policy.viewer
+}
