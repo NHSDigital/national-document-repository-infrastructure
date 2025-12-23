606c5232ed2577f5a67b2a26caef378059eadb9e
jameslinnell <jimlinnell@gmail.com>
2025-12-23 16:53:51 +0000

    [NDR-322] Remove PDM table from lambdas

--- infrastructure/lambda-get-document-fhir.tf
+++ infrastructure/lambda-get-document-fhir.tf
@@ -39,7 +39,6 @@ module "get-doc-fhir-lambda" {
   iam_role_policy_documents = [
     module.ndr-app-config.app_config_policy,
     module.lloyd_george_reference_dynamodb_table.dynamodb_read_policy_document,
-    module.pdm_dynamodb_table.dynamodb_read_policy_document,
     module.core_dynamodb_table.dynamodb_read_policy_document,
     aws_iam_policy.ssm_access_policy.policy,
     aws_iam_policy.mtls_access_ssm_policy.policy,
@@ -58,15 +57,12 @@ module "get-doc-fhir-lambda" {
     WORKSPACE                  = terraform.workspace
     ENVIRONMENT                = var.environment
     PRESIGNED_ASSUME_ROLE      = aws_iam_role.get_fhir_doc_presign_url_role.arn
-    LLOYD_GEORGE_DYNAMODB_NAME = module.lloyd_george_reference_dynamodb_table.table_name
-    PDM_DYNAMODB_NAME          = module.pdm_dynamodb_table.table_name
     OIDC_CALLBACK_URL          = contains(["prod"], terraform.workspace) ? "https://${var.domain}/auth-callback" : "https://${terraform.workspace}.${var.domain}/auth-callback"
     CLOUDFRONT_URL             = aws_cloudfront_distribution.s3_presign_mask.domain_name
     PDS_FHIR_IS_STUBBED        = local.is_sandbox
   }
   depends_on = [
     aws_api_gateway_method.get_document_reference,
-    module.pdm_dynamodb_table,
     module.lloyd_george_reference_dynamodb_table,
     module.core_dynamodb_table,
   ]
