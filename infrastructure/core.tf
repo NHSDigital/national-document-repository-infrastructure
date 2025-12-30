6deef3b9af0ba611859fa7fc5579bbe61ca1934e
jameslinnell <jimlinnell@gmail.com>
2025-12-30 09:39:34 +0000

    [NDR-343] Allow SSM param for non sandboxes only

--- infrastructure/core.tf
+++ infrastructure/core.tf
@@ -1,12 +1,13 @@
 # A set of resources that do not belong to one workspace but are shared across the environment.
 
 module "ssm_param_mtls_common_names" {
+  count                = local.is_sandbox ? 0 : 1
   source               = "./modules/ssm_parameter"
   environment          = var.environment
   owner                = var.owner
   name                 = "mtls_common_names"
   type                 = "SecureString"
-  description          = "A list of mtls common names that will be used to determine authorisation and resources used."
+  description          = "A list of mtls common names that will be used to determine authorisation and resources."
   value                = "REPLACE_ME"
   key_id               = module.pdm_encryption_key.id
   ignore_value_changes = true
