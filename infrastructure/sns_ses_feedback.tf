39f20eff983b7fa17f4dac8b22a82ff29d178f9d
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-01-14 11:57:35 +0000

    [PRMP-1057] testing

--- infrastructure/sns_ses_feedback.tf
+++ infrastructure/sns_ses_feedback.tf
@@ -1,24 +1,25 @@
-module "ses_feedback_topic" {
-  source                = "./modules/sns"
-  topic_name            = "ses-feedback-events"
-  topic_protocol        = "lambda"
-  topic_endpoint        = module.ses-feedback-monitor-lambda.lambda_arn
-  sns_encryption_key_id = module.sns_encryption_key.kms_arn
-  raw_message_delivery  = false
-  delivery_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "DefaultOwnerPermissions"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        }
-        Action   = "SNS:*"
-        Resource = "*"
+delivery_policy = jsonencode({
+  Version = "2012-10-17"
+  Statement = [
+    {
+      Sid    = "DefaultOwnerPermissions"
+      Effect = "Allow"
+      Principal = {
+        AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
       }
-    ]
-  })
-  enable_ses_publish    = true
-  ses_source_account_id = data.aws_caller_identity.current.account_id
-}
+
+      Action = [
+        "SNS:GetTopicAttributes",
+        "SNS:SetTopicAttributes",
+        "SNS:AddPermission",
+        "SNS:RemovePermission",
+        "SNS:DeleteTopic",
+        "SNS:Subscribe",
+        "SNS:ListSubscriptionsByTopic",
+        "SNS:Publish"
+      ]
+
+      Resource = "*"
+    }
+  ]
+})
