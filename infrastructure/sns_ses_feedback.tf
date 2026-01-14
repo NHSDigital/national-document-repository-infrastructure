55fb7e4989dc41851bc12f0ac218a6561ac10ca3
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-01-14 12:52:29 +0000

    [PRMP-1057] testing

--- infrastructure/sns_ses_feedback.tf
+++ infrastructure/sns_ses_feedback.tf
@@ -1,25 +1,35 @@
-delivery_policy = jsonencode({
-  Version = "2012-10-17"
-  Statement = [
-    {
-      Sid    = "DefaultOwnerPermissions"
-      Effect = "Allow"
-      Principal = {
-        AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+module "ses_feedback_topic" {
+  source                = "./modules/sns"
+  topic_name            = "ses-feedback-events"
+  topic_protocol        = "lambda"
+  topic_endpoint        = module.ses-feedback-monitor-lambda.lambda_arn
+  sns_encryption_key_id = module.sns_encryption_key.kms_arn
+  raw_message_delivery = false
+
+  delivery_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "DefaultOwnerPermissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = [
+          "SNS:GetTopicAttributes",
+          "SNS:SetTopicAttributes",
+          "SNS:AddPermission",
+          "SNS:RemovePermission",
+          "SNS:DeleteTopic",
+          "SNS:Subscribe",
+          "SNS:ListSubscriptionsByTopic",
+          "SNS:Publish"
+        ]
+        Resource = "*"
       }
+    ]
+  })
 
-      Action = [
-        "SNS:GetTopicAttributes",
-        "SNS:SetTopicAttributes",
-        "SNS:AddPermission",
-        "SNS:RemovePermission",
-        "SNS:DeleteTopic",
-        "SNS:Subscribe",
-        "SNS:ListSubscriptionsByTopic",
-        "SNS:Publish"
-      ]
-
-      Resource = "*"
-    }
-  ]
-})
+  enable_ses_publish    = true
+  ses_source_account_id = data.aws_caller_identity.current.account_id
+}
