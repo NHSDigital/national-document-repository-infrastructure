b3851e2d165cb311092a3c9a6fcc42b2a1869efd
Tim Knight <tim.knight@airelogic.com>
2026-01-26 15:07:01 +0000

    [ndr-372] Added roles per account

--- infrastructure/service-quota.tf
+++ infrastructure/service-quota.tf
@@ -9,6 +9,17 @@ resource "aws_servicequotas_service_quota" "customer_managed_policies_per_accoun
   provider = aws.us_east_1
 }
 
+resource "aws_servicequotas_service_quota" "customer_managed_roles_per_account" {
+  count = local.is_sandbox || local.is_production ? 0 : 1
+
+  service_code = "iam"
+  quota_code   = "L-FE177D64"
+  value        = 5000
+
+  provider = aws.us_east_1
+}
+
+# https://docs.aws.amazon.com/lambda/latest/dg/gettingstarted-limits.html#compute-and-storage
 resource "aws_servicequotas_service_quota" "lambda_concurrent_executions_increase" {
   count = local.is_sandbox || local.is_production ? 0 : 1 # The service code for AWS Lambda
 
