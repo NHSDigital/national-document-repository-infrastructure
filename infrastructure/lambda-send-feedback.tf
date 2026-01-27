0f4a6cef5d8cdd204a76f15b19211878a65358eb
SWhyteAnswer <sam.whyte@answerdigital.com>
2026-01-27 16:12:49 +0000

    [PRMP-1215] missed a contains tests oops

--- infrastructure/lambda-send-feedback.tf
+++ infrastructure/lambda-send-feedback.tf
@@ -30,7 +30,7 @@ module "send-feedback-gateway" {
   gateway_path        = "Feedback"
   authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
   require_credentials = true
-  origin              = contains(["prod"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
+  origin              = contains(["prod", "ndr-test"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
 }
 
 module "send-feedback-alarm" {
