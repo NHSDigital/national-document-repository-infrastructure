5d84817b63c6ccb62d3dc6987e8c5f889a9c5dba
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-01-27 18:17:00 +0000

    [PRMP-1057-3] update

--- infrastructure/ssm_reporting.tf
+++ infrastructure/ssm_reporting.tf
@@ -11,13 +11,13 @@ resource "aws_ssm_parameter" "reporting_ses_from_address" {
 
 data "aws_ssm_parameter" "reporting_ses_from_address" {
   count = local.is_shared_workspace ? 0 : 1
-  name = "/prs/${var.environment}/user-input/reporting-ses-from-address"
+  name  = "/prs/${var.environment}/user-input/reporting-ses-from-address"
 }
 locals {
   reporting_ses_from_address_value = (
     local.is_shared_workspace
-      ? aws_ssm_parameter.reporting_ses_from_address[0].value
-      : data.aws_ssm_parameter.reporting_ses_from_address[0].value
+    ? aws_ssm_parameter.reporting_ses_from_address[0].value
+    : data.aws_ssm_parameter.reporting_ses_from_address[0].value
   )
 }
 
