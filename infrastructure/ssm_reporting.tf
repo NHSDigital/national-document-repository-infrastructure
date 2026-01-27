59408a1f7fe05875d1a40e917d2f7f343f91f599
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-01-27 18:30:51 +0000

    [PRMP-1057-3] update

--- infrastructure/ssm_reporting.tf
+++ infrastructure/ssm_reporting.tf
@@ -9,15 +9,22 @@ resource "aws_ssm_parameter" "reporting_ses_from_address" {
   }
 }
 
-data "aws_ssm_parameter" "reporting_ses_from_address" {
+data "aws_ssm_parameters_by_path" "reporting_user_input" {
   count = local.is_shared_workspace ? 0 : 1
-  name  = "/prs/${var.environment}/user-input/reporting-ses-from-address"
+  path            = "/prs/${var.environment}/user-input"
+  recursive       = false
+  with_decryption = true
 }
+
 locals {
+  reporting_ses_from_address_name = "/prs/${var.environment}/user-input/reporting-ses-from-address"
+  reporting_user_input_params = local.is_shared_workspace ? {} : zipmap(
+    data.aws_ssm_parameters_by_path.reporting_user_input[0].names,
+    data.aws_ssm_parameters_by_path.reporting_user_input[0].values
+  )
   reporting_ses_from_address_value = (
     local.is_shared_workspace
     ? aws_ssm_parameter.reporting_ses_from_address[0].value
-    : data.aws_ssm_parameter.reporting_ses_from_address[0].value
+    : lookup(local.reporting_user_input_params, local.reporting_ses_from_address_name, "ndr-reports@${var.domain}")
   )
 }
-
