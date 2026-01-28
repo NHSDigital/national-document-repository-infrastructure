18f10173fe70816c4dd598737ea72137b8b8dd51
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-01-28 13:58:28 +0000

    [PRMP-1058] update

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -375,23 +375,26 @@ data "aws_iam_policy_document" "reporting_ses" {
     }
   }
 
-  statement {
-    sid    = "SESAccessRecipientIdentitySandbox"
-    effect = "Allow"
+  dynamic "statement" {
+    for_each = local.is_sandbox ? [1] : []
+    content {
+      sid    = "SESAccessRecipientIdentitySandbox"
+      effect = "Allow"
 
-    actions = [
-      "ses:SendEmail",
-      "ses:SendRawEmail"
-    ]
+      actions = [
+        "ses:SendEmail",
+        "ses:SendRawEmail"
+      ]
 
-    resources = [
-      "arn:aws:ses:${var.region}:${data.aws_caller_identity.current.account_id}:identity/*",
-    ]
+      resources = [
+        "arn:aws:ses:${var.region}:${data.aws_caller_identity.current.account_id}:identity/*",
+      ]
 
-    condition {
-      test     = "StringEquals"
-      variable = "ses:FromAddress"
-      values   = [local.reporting_ses_from_address_value]
+      condition {
+        test     = "StringEquals"
+        variable = "ses:FromAddress"
+        values   = [local.reporting_ses_from_address_value]
+      }
     }
   }
 }
@@ -419,4 +422,3 @@ data "aws_iam_policy_document" "ses_feedback_s3_put" {
     ]
   }
 }
-
