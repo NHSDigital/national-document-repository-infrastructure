f67697dc2ef08d5cfc770d67b37b6b204cd7cde3
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-01-29 16:57:19 +0000

    [PRMP-1057-3] fixed comments

--- infrastructure/ssm_reporting.tf
+++ infrastructure/ssm_reporting.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "reporting_ses_from_address" {
   count = local.is_shared_workspace ? 1 : 0
   name  = "/prs/${var.environment}/user-input/reporting-ses-from-address"
   type  = "String"
-  value = "ndr-reports@${local.reporting_from_domain}"
+  value = local.reporting_ses_from_address_default
 
   tags = {
     Name = "/prs/${var.environment}/user-input/reporting-ses-from-address"
@@ -22,7 +22,9 @@ locals {
     : "${var.shared_infra_workspace}.${var.domain}"
   )
 
-  reporting_ses_from_address_name = "/prs/${var.environment}/user-input/reporting-ses-from-address"
+  reporting_ses_from_address_name    = "/prs/${var.environment}/user-input/reporting-ses-from-address"
+  reporting_ses_from_address_default = "ndr-reports@${local.reporting_from_domain}"
+
   reporting_user_input_params = local.is_shared_workspace ? {} : zipmap(
     data.aws_ssm_parameters_by_path.reporting_user_input[0].names,
     data.aws_ssm_parameters_by_path.reporting_user_input[0].values
@@ -33,7 +35,7 @@ locals {
     : lookup(
       local.reporting_user_input_params,
       local.reporting_ses_from_address_name,
-      "ndr-reports@${local.reporting_from_domain}"
+      local.reporting_ses_from_address_default
     )
   )
 }
