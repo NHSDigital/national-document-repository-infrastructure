2ed791e3780b77f73660ea73f721eab8124aa6d4
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-02-02 09:44:01 +0000

    [PRMP-1058] testing a simpler version

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -387,14 +387,14 @@ data "aws_iam_policy_document" "reporting_ses" {
 #   policy_arn = aws_iam_policy.reporting_ses_send.arn
 # }
 #
-# data "aws_iam_policy_document" "ses_feedback_s3_put" {
-#   statement {
-#     effect = "Allow"
-#     actions = [
-#       "s3:PutObject"
-#     ]
-#     resources = [
-#       "${module.ses-feedback-store.bucket_arn}/ses-feedback/*"
-#     ]
-#   }
-# }
+data "aws_iam_policy_document" "ses_feedback_s3_put" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = [
+      "${module.ses-feedback-store.bucket_arn}/ses-feedback/*"
+    ]
+  }
+}
