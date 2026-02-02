f62d738631654d2ca8091dc3a96dd130fe2bcd2b
PedroSoaresNHS <pedro.soares1@nhs.net>
2026-02-02 14:42:07 +0000

    [PRMP-1057-3] testing

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -365,7 +365,8 @@ data "aws_iam_policy_document" "reporting_ses" {
     ]
 
     resources = [
-      "arn:aws:ses:${var.region}:${data.aws_caller_identity.current.account_id}:identity/*"
+      "arn:aws:ses:${var.region}:${data.aws_caller_identity.current.account_id}:identity/*",
+      # "arn:aws:ses:${var.region}:${data.aws_caller_identity.current.account_id}:configuration-set/${aws_ses_configuration_set.reporting.name}",
     ]
 
     condition {
@@ -375,4 +376,3 @@ data "aws_iam_policy_document" "reporting_ses" {
     }
   }
 }
-
