b51db70126d25217c775326ba2c502b46930082b
Adam Whiting <adam.whiting3@nhs.net>
2026-02-04 08:44:50 +0000

    [PRMP-1377] Enable RUM above dev environment (#584)

--- infrastructure/moved-resources-v1.6.11.tf
+++ infrastructure/moved-resources-v1.6.11.tf
@@ -17,3 +17,33 @@ moved {
   from = aws_cloudfront_cache_policy.nocache
   to   = aws_cloudfront_cache_policy.nocache[0]
 }
+
+moved {
+  from = aws_iam_role.cognito_unauthenticated[0]
+  to   = aws_iam_role.cognito_unauthenticated
+}
+
+moved {
+  from = aws_iam_policy.cloudwatch_rum_cognito_access[0]
+  to   = aws_iam_policy.cloudwatch_rum_cognito_access
+}
+
+moved {
+  from = aws_iam_role_policy_attachment.cloudwatch_rum_cognito_unauth[0]
+  to   = aws_iam_role_policy_attachment.cloudwatch_rum_cognito_unauth
+}
+
+moved {
+  from = aws_cognito_identity_pool.cloudwatch_rum[0]
+  to   = aws_cognito_identity_pool.cloudwatch_rum
+}
+
+moved {
+  from = aws_cognito_identity_pool_roles_attachment.cloudwatch_rum[0]
+  to   = aws_cognito_identity_pool_roles_attachment.cloudwatch_rum
+}
+
+moved {
+  from = aws_rum_app_monitor.ndr[0]
+  to   = aws_rum_app_monitor.ndr
+}
