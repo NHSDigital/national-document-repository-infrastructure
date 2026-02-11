245b116623a737dddda1847c1abf08acbbe00664
Kamen Bachvarov <kamen.bachvaror1@nhs.net>
2026-02-11 17:28:49 +0000

    WIP add authoriser to version ep

--- infrastructure/lambda-search-document-reference-history.tf
+++ infrastructure/lambda-search-document-reference-history.tf
@@ -76,3 +76,11 @@ resource "aws_api_gateway_integration" "get_document_reference_version_integrati
   type                    = "AWS_PROXY"
   uri                     = module.get-doc-ref-lambda.invoke_arn
 }
+
+resource "aws_api_gateway_method" "get_document_reference_version_method" {
+  rest_api_id   = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  resource_id   = module.document_reference_version_gateway.gateway_resource_id
+  http_method   = "GET"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.repo_authoriser.id
+}
