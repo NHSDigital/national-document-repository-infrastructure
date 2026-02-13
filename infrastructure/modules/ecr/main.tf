c914eeaf79c811d5cb0d32a30ba90bf50725662a
Tim Knight <tim.knight@airelogic.com>
2026-02-13 12:03:18 +0000

    [ndr-373] formatting

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -18,29 +18,30 @@ resource "aws_ecr_repository" "ndr-ecr" {
 resource "aws_ecr_lifecycle_policy" "ndr_ecr_lifecycle_policy" {
   repository = aws_ecr_repository.ndr-ecr.name
   policy     = <<EOF
-  {
-    "rules": [
-      {
-        "rulePriority": 1,
-        "description": "Keep most 5 most recent tagged images",
-        "selection": {
-          "tagStatus": "tagged",
-          "countType": "imageCountMoreThan",
-          "countNumber": 5
-        },
-        "action": {
-          "type": "expire"
-        }
-      {
-        "rulePriority": 2,
-        "description": "Expire images older than 7 days",
-        "selection": {
-          "tagStatus": "untagged",
-          "countType": "sinceImagePushed",
-          "countUnit": "days",
-          "countNumber": 7
-        },
-        "action": {
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep most 5 most recent tagged images",
+      "selection": {
+        "tagStatus": "tagged",
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    },
+    {
+      "rulePriority": 2,
+      "description": "Expire images older than 7 days",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 7
+      },
+      "action": {
         "type": "expire"
       }
     }
