469b5ed307a281e0b23d905bb61cd23e5affb763
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2026-02-16 14:58:11 +0000

    [PRMP-1445] add user restriction gateways

---
+++ infrastructure/gateway-user-restrictions.tf
@@ -0,0 +1,39 @@
+module "user_restrictions_gateway" {
+  source              = "./modules/gateway"
+  api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api
+  parent_id           = aws_api_gateway_rest_api.ndr_doc_store_api.root_resource_id
+  http_methods        = ["GET", "POST"]
+  authorization       = "CUSTOM"
+  gateway_path        = "UserRestriction"
+  authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
+  require_credentials = true
+  origin              = contains(["prod"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
+}
+
+module "user_restriction_id_gateway" {
+  source              = "./modules/gateway"
+  api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  parent_id           = module.user_restrictions_gateway.gateway_resource_id
+  http_methods        = ["GET", "DELETE"]
+  gateway_path        = "{id}"
+  authorization       = "CUSTOM"
+  authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
+  require_credentials = true
+  origin              = contains(["prod"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
+
+  request_parameters = {
+    "method.request.path.id" = true
+  }
+}
+
+module "user_restrictions_user_search_gateway" {
+  source              = "./modules/gateway"
+  api_gateway_id      = aws_api_gateway_rest_api.ndr_doc_store_api.id
+  parent_id           = module.user_restrictions_gateway.gateway_resource_id
+  http_methods        = ["GET"]
+  gateway_path        = "SearchUser"
+  authorization       = "CUSTOM"
+  authorizer_id       = aws_api_gateway_authorizer.repo_authoriser.id
+  require_credentials = true
+  origin              = contains(["prod"], terraform.workspace) ? "'https://${var.domain}'" : "'https://${terraform.workspace}.${var.domain}'"
+}
