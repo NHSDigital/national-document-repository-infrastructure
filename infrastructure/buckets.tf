b2e5c6b16665ac46ee168c9135b6db83eb085a09
Robert Gaskin <106234256+robg-test@users.noreply.github.com>
2026-02-26 09:08:32 +0000

    [PRMP-739] Add Expiry on Delete Markers Bucket Wide (#620)

--- infrastructure/buckets.tf
+++ infrastructure/buckets.tf
@@ -229,6 +229,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "staging-store-lifecycle-rules"
     id     = "Delete non-current versions in staging bucket that have existed for 24 hours"
     status = "Enabled"
 
+    expiration {
+      expired_object_delete_marker = true
+    }
+
     noncurrent_version_expiration {
       noncurrent_days = 1
     }
