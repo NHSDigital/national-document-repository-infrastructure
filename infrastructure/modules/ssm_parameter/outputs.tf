2a3be3a4556579bc71f41467c2f9d4281a0bfc8f
steph-torres-nhs <173282814+steph-torres-nhs@users.noreply.github.com>
2026-02-26 16:49:40 +0000

    [PRMP-1475] update output

--- infrastructure/modules/ssm_parameter/outputs.tf
+++ infrastructure/modules/ssm_parameter/outputs.tf
@@ -1,4 +1,4 @@
-output "ssm_ignore_change_value" {
-  value       = aws_ssm_parameter.secret_ignore_value_changes.value
+output "ssm_value" {
+  value       = var.ignore_value_changes ? aws_ssm_parameter.secret_ignore_value_changes[0].value : aws_ssm_parameter.secret[0].value
   description = "The value of ssm parameter"
 }
