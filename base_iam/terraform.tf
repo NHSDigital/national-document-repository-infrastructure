d1b59984695117ee56a58fd55cf15347f6837073
Tim Knight <tim.knight@airelogic.com>
2026-03-02 11:53:54 +0000

    [ndr-373] reverting temp tf change

--- base_iam/terraform.tf
+++ base_iam/terraform.tf
@@ -1,18 +1,17 @@
-# TODO: REVERT TO ORIGINAL BACKEND CONFIGURATION
-# # Based on settings in infrastructure/main.tf
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 6.0"
-#     }
-#   }
+# Based on settings in infrastructure/main.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 
 
-#   backend "s3" {
-#     use_lockfile = true
-#     region       = "eu-west-2"
-#     key          = "ndr_base_iam/terraform.tfstate"
-#     encrypt      = true
-#   }
-# }
+  backend "s3" {
+    use_lockfile = true
+    region       = "eu-west-2"
+    key          = "ndr_base_iam/terraform.tfstate"
+    encrypt      = true
+  }
+}
