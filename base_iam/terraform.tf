dec01386c943852490347f1ab9a63d3daa9d8873
Tim Knight <tim.knight@airelogic.com>
2026-03-02 09:32:51 +0000

    [ndr-353] temp change to exract config

--- base_iam/terraform.tf
+++ base_iam/terraform.tf
@@ -1,17 +1,18 @@
-# Based on settings in infrastructure/main.tf
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
+# TODO: REVERT TO ORIGINAL BACKEND CONFIGURATION
+# # Based on settings in infrastructure/main.tf
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 6.0"
+#     }
+#   }
 
 
-  backend "s3" {
-    use_lockfile = true
-    region       = "eu-west-2"
-    key          = "ndr_base_iam/terraform.tfstate"
-    encrypt      = true
-  }
-}
+#   backend "s3" {
+#     use_lockfile = true
+#     region       = "eu-west-2"
+#     key          = "ndr_base_iam/terraform.tfstate"
+#     encrypt      = true
+#   }
+# }
