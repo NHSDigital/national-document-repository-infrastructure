19534b0697a18b685192ae02c82bb1587f934b1e
chrisbloe <kris.bloe2@nhs.net>
2026-03-03 10:14:55 +0000

    Fixing the domain

--- infrastructure/modules/ses/main.tf
+++ infrastructure/modules/ses/main.tf
@@ -30,8 +30,8 @@ resource "aws_ses_domain_identity_verification" "ndr_ses_domain_verification" {
 
 resource "aws_ses_domain_mail_from" "reporting" {
   count            = var.enable ? 1 : 0
-  domain           = module.ses.domain_identity
-  mail_from_domain = "mail.${terraform.workspace}.${var.domain}"
+  domain           = aws_ses_domain_identity.ndr_ses[0].domain
+  mail_from_domain = "mail.${aws_ses_domain_identity.ndr_ses[0].domain}"
 
   behavior_on_mx_failure = "UseDefaultValue"
 }
@@ -39,7 +39,7 @@ resource "aws_ses_domain_mail_from" "reporting" {
 resource "aws_route53_record" "ses_mail_from_mx" {
   count   = var.enable ? 1 : 0
   zone_id = var.zone_id
-  name    = "mail.${terraform.workspace}.${var.domain}"
+  name    = "mail.${aws_ses_domain_identity.ndr_ses[0].domain}"
   type    = "MX"
   ttl     = 600
 
@@ -51,7 +51,7 @@ resource "aws_route53_record" "ses_mail_from_mx" {
 resource "aws_route53_record" "ses_mail_from_spf" {
   count   = var.enable ? 1 : 0
   zone_id = var.zone_id
-  name    = "mail.${terraform.workspace}.${var.domain}"
+  name    = "mail.${aws_ses_domain_identity.ndr_ses[0].domain}"
   type    = "TXT"
   ttl     = 600
 
