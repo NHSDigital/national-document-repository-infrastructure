4238509a74a2a7976b3c29cb99aebc76b8d03284
robg-test <106234256+robg-test@users.noreply.github.com>
2026-03-06 10:14:40 +0000

    [PRMP-1215] Add module path?

--- infrastructure/moved-resources-v1.6.14.tf
+++ infrastructure/moved-resources-v1.6.14.tf
@@ -1,4 +1,4 @@
 moved {
-  from = module.aws_route53_record.ndr_fargate_record
-  to   = module.aws_route53_record.ndr_fargate_record_cname[0]
+  from = module.route53_fargate_ui.aws_route53_record.ndr_fargate_record
+  to   = module.route53_fargate_ui.aws_route53_record.ndr_fargate_record_cname[0]
 }
