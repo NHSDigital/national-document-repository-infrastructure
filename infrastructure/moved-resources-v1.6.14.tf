08e6424f71279fa08fdfbbb4468887ff203e1027
robg-test <106234256+robg-test@users.noreply.github.com>
2026-03-06 10:08:30 +0000

    [PRMP-1215] Switch to module?

--- infrastructure/moved-resources-v1.6.14.tf
+++ infrastructure/moved-resources-v1.6.14.tf
@@ -1,4 +1,4 @@
 moved {
-  from = resource.aws_route53_record.ndr_fargate_record
-  to   = resource.aws_route53_record.ndr_fargate_record_cname[0]
+  from = module.aws_route53_record.ndr_fargate_record
+  to   = module.aws_route53_record.ndr_fargate_record_cname[0]
 }
