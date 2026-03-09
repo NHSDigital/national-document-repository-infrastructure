ab4f993f5a11eace51deaf237e928cbad0029a09
chrisbloe <kris.bloe2@nhs.net>
2026-03-09 16:46:40 +0000

    Correcting indexes

--- infrastructure/modules/ses/main.tf
+++ infrastructure/modules/ses/main.tf
@@ -3,9 +3,9 @@ resource "aws_ses_domain_identity" "ndr_ses" {
 }
 
 resource "aws_ses_domain_dkim" "ndr_dkim" {
-  domain = aws_ses_domain_identity.ndr_ses[0].domain
+  domain = aws_ses_domain_identity.ndr_ses.domain
 
-  depends_on = [aws_ses_domain_identity.ndr_ses[0]]
+  depends_on = [aws_ses_domain_identity.ndr_ses]
 }
 
 resource "aws_route53_record" "ndr_ses_dkim_record" {
@@ -15,27 +15,27 @@ resource "aws_route53_record" "ndr_ses_dkim_record" {
   name    = var.is_sandbox ? "${aws_ses_domain_dkim.ndr_dkim[0].dkim_tokens[count.index]}._domainkey.${terraform.workspace}.dev" : "${aws_ses_domain_dkim.ndr_dkim[0].dkim_tokens[count.index]}._domainkey.${terraform.workspace}"
   type    = "CNAME"
   ttl     = 1800
-  records = ["${aws_ses_domain_dkim.ndr_dkim[0].dkim_tokens[count.index]}.dkim.amazonses.com"]
+  records = ["${aws_ses_domain_dkim.ndr_dkim.dkim_tokens[count.index]}.dkim.amazonses.com"]
 
-  depends_on = [aws_ses_domain_dkim.ndr_dkim[0]]
+  depends_on = [aws_ses_domain_dkim.ndr_dkim]
 }
 
 resource "aws_ses_domain_identity_verification" "ndr_ses_domain_verification" {
-  domain = aws_ses_domain_identity.ndr_ses[0].domain
+  domain = aws_ses_domain_identity.ndr_ses.domain
 
-  depends_on = [aws_route53_record.ndr_ses_dkim_record[0]]
+  depends_on = [aws_route53_record.ndr_ses_dkim_record]
 }
 
 resource "aws_ses_domain_mail_from" "reporting" {
-  domain           = aws_ses_domain_identity.ndr_ses[0].domain
-  mail_from_domain = "mail.${aws_ses_domain_identity.ndr_ses[0].domain}"
+  domain           = aws_ses_domain_identity.ndr_ses.domain
+  mail_from_domain = "mail.${aws_ses_domain_identity.ndr_ses.domain}"
 
   behavior_on_mx_failure = "UseDefaultValue"
 }
 
 resource "aws_route53_record" "ses_mail_from_mx" {
   zone_id = var.zone_id
-  name    = "mail.${aws_ses_domain_identity.ndr_ses[0].domain}"
+  name    = "mail.${aws_ses_domain_identity.ndr_ses.domain}"
   type    = "MX"
   ttl     = 600
 
@@ -46,7 +46,7 @@ resource "aws_route53_record" "ses_mail_from_mx" {
 
 resource "aws_route53_record" "ses_mail_from_spf" {
   zone_id = var.zone_id
-  name    = "mail.${aws_ses_domain_identity.ndr_ses[0].domain}"
+  name    = "mail.${aws_ses_domain_identity.ndr_ses.domain}"
   type    = "TXT"
   ttl     = 600
 
@@ -57,7 +57,7 @@ resource "aws_route53_record" "ses_mail_from_spf" {
 
 resource "aws_route53_record" "dmarc" {
   zone_id = var.zone_id
-  name    = "_dmarc.${aws_ses_domain_identity.ndr_ses[0].domain}"
+  name    = "_dmarc.${aws_ses_domain_identity.ndr_ses.domain}"
   type    = "TXT"
   ttl     = 300
 
--- infrastructure/modules/ses/output.tf
+++ infrastructure/modules/ses/output.tf
@@ -1,3 +1,3 @@
 output "report_email_address" {
-  value = "ndr-reports@${aws_ses_domain_identity.ndr_ses[0].domain}"
+  value = "ndr-reports@${aws_ses_domain_identity.ndr_ses.domain}"
 }
