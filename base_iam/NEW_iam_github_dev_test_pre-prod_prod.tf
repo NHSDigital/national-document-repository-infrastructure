c0bad9f4e223742ff7b6f3662d81705a8b7fe9ba
Tim Knight <tim.knight@airelogic.com>
2026-03-12 16:29:47 +0000

    [ndr-387] added comments and better naming of split  policies

--- base_iam/NEW_iam_github_dev_test_pre-prod_prod.tf
+++ base_iam/NEW_iam_github_dev_test_pre-prod_prod.tf
@@ -1,12 +1,14 @@
-resource "aws_iam_role_policy_attachment" "github_actions_policy_dev_test_pre-prod_prod" {
+# POLICY SPLIT INTO 3 PARTS TO AVOID HITTING THE 6,144 CHARACTER LIMIT FOR AWS IAM POLICIES
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy_dev_test_pre-prod_prod_1" {
   count      = local.is_dev_test_pre-prod_prod ? 1 : 0
   role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.github_actions_policy_dev_test_pre-prod_prod[0].arn
+  policy_arn = aws_iam_policy.github_actions_policy_dev_test_pre-prod_prod_1[0].arn
 }
 
-resource "aws_iam_policy" "github_actions_policy_dev_test_pre-prod_prod" {
+resource "aws_iam_policy" "github_actions_policy_dev_test_pre-prod_prod_1" {
   count = local.is_dev_test_pre-prod_prod ? 1 : 0
-  name  = "${terraform.workspace}-github-actions-policy-dev_test_pre-prod_prod"
+  name  = "${terraform.workspace}-github-actions-policy-dev_test_pre-prod_prod_1"
   path  = "/"
   policy = jsonencode({
     Version = "2012-10-17"
@@ -111,7 +113,6 @@ resource "aws_iam_policy" "github_actions_policy_dev_test_pre-prod_prod" {
 }
 
 
-# ABOVE POLICY SPLIT TO AVOID HITTING THE 6,144 CHARACTER LIMIT FOR AWS IAM POLICIES
 resource "aws_iam_role_policy_attachment" "github_actions_policy_dev_test_pre-prod_prod_2" {
   count      = local.is_dev_test_pre-prod_prod ? 1 : 0
   role       = aws_iam_role.github_actions.name
@@ -264,7 +265,6 @@ resource "aws_iam_policy" "github_actions_policy_dev_test_pre-prod_prod_2" {
 }
 
 
-# ABOVE POLICY SPLIT TO AVOID HITTING THE 6,144 CHARACTER LIMIT FOR AWS IAM POLICIES
 resource "aws_iam_role_policy_attachment" "github_actions_policy_dev_test_pre-prod_prod_3" {
   count      = local.is_dev_test_pre-prod_prod ? 1 : 0
   role       = aws_iam_role.github_actions.name
